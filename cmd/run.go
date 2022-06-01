3b020189aaa385a2490a4546d226b692b4015b78
peterhoward42 <peter@keel.xyz>
2022-06-01 23:20:51 +0100

    Get a schema directory watcher firing

--- cmd/run.go
+++ cmd/run.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"io"
 	"os"
 
 	"github.com/docker/docker/api/types"
@@ -13,6 +12,8 @@ import (
 	"github.com/teamkeel/keel/cmd/formatter"
 	"github.com/teamkeel/keel/schema"
 	"github.com/teamkeel/keel/schema/validation"
+
+	"github.com/fsnotify/fsnotify"
 )
 
 type runCommand struct {
@@ -63,6 +64,25 @@ func commandImplementation(cmd *cobra.Command, args []string) error {
 	c.outputFormatter.Write("Starting PostgreSQL")
 	bringUpPostgres()
 
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return fmt.Errorf("error creating schema change watcher: %v", err)
+	}
+	defer watcher.Close()
+
+	go reactToSchemaChanges(watcher)
+
+	err = watcher.Add(inputDir)
+	if err != nil {
+		return fmt.Errorf("error specifying directory to schema watcher: %v", err)
+	}
+
+	c.outputFormatter.Write(fmt.Sprintf("Waiting for a schema file to change in %s ...\n", inputDir))
+
+	// Block until the CLI process is terminated.
+	ch := make(chan bool)
+	<-ch
+
 	return nil
 }
 
@@ -91,7 +111,7 @@ func bringUpPostgres() error {
 		panic(err)
 	}
 	defer out.Close()
-	io.Copy(os.Stdout, out)
+	// io.Copy(os.Stdout, out)
 
 	containerConfig := &container.Config{
 		Image: imageName,
@@ -106,4 +126,26 @@ func bringUpPostgres() error {
 		panic(err)
 	}
 	return nil
+}
+
+func reactToSchemaChanges(watcher *fsnotify.Watcher) {
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				fmt.Printf("XXXX seems the watcher event channel got closed\n")
+				return
+			}
+			if event.Op&fsnotify.Write == fsnotify.Write {
+				fmt.Printf("XXXX detected that %s changed\n", event.Name)
+			}
+
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				fmt.Printf("XXXX seems the watcher error channel got closed\n")
+				return
+			}
+			fmt.Printf("XXXX error received on watcher error channel: %v\n", err)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/alecthomas/participle/v2 v2.0.0-alpha8
 	github.com/docker/docker v20.10.16+incompatible
+	github.com/fsnotify/fsnotify v1.5.4
 	github.com/iancoleman/strcase v0.2.0
 	github.com/ka-weihe/fast-levenshtein v0.0.0-20201227151214-4c99ee36a1ba
 	github.com/nsf/jsondiff v0.0.0-20210926074059-1e845ec5d249
@@ -33,7 +34,7 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/docker/go-connections v0.4.0
+	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
