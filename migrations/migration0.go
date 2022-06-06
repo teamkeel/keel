00f88f70ddb89954f9f8fbeca2c4ec5174ac3693
peterhoward42 <peter@keel.xyz>
2022-06-06 16:08:12 +0100

    Make a proper test for first baby steps SQL generation

--- migrations/migration0.go
+++ migrations/migration0.go
@@ -27,16 +27,31 @@ func NewMigration0(schema *proto.Schema) *Migration0 {
 }
 
 func (m0 *Migration0) GenerateSQL() {
-	// First drop the entire existing database if it exists.
-	s := fmt.Sprintf("DROP DATABASE IF EXISTS %s;", databaseName)
-	m0.SQL = append(m0.SQL, s)
+	m0.appendFrontMatter()
 
-	// todo add db creation and selection
-
-	// now the model tables
 	for _, model := range m0.schema.Models {
 		m0.appendCreateModel(model)
 	}
+	// todo - similar for API's, Enums, etc.
+}
+
+func (m0 *Migration0) appendFrontMatter() {
+	// todo this function alongside appendCreateModel should be DRY-ed up
+	// before adding any more.
+	templateData := map[string]string{
+		"DbName": "keel",
+	}
+
+	tmpl, err := template.New("").Parse(templateInit)
+	if err != nil {
+		panic(fmt.Sprintf("error parsing template: %v", err))
+	}
+	output := bytes.Buffer{}
+	err = tmpl.Execute(&output, templateData)
+	if err != nil {
+		panic(fmt.Sprintf("error executing template: %v", err))
+	}
+	m0.SQL = append(m0.SQL, output.String())
 }
 
 func (m0 *Migration0) appendCreateModel(model *proto.Model) {
@@ -49,7 +64,7 @@ func (m0 *Migration0) appendCreateModel(model *proto.Model) {
 		templateData.Columns = append(templateData.Columns, m0.column(field))
 	}
 
-	tmpl, err := template.New("foo").Parse(templateCreateTable)
+	tmpl, err := template.New("").Parse(templateCreateTable)
 	if err != nil {
 		panic(fmt.Sprintf("error parsing template: %v", err))
 	}
@@ -80,7 +95,12 @@ type column struct {
 	Type string
 }
 
-// todo: need to add [] when field type is a list
+// todos:
+// add [] when field type is a list
+// add field constraints
 const templateCreateTable string = `CREATE TABLE {{.Name}} (
 {{range .Columns}}  {{.Name}} {{.Type}},
 {{end}});`
+
+const templateInit string = `DROP DATABASE IF EXISTS {{.DbName}};
+CREATE DATABASE {{.DbName}};`
--- migrations/migration0_test.go
+++ migrations/migration0_test.go
@@ -2,27 +2,43 @@ package migrations
 
 import (
 	"fmt"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"github.com/teamkeel/keel/proto"
 )
 
-func TestItCompiles(t *testing.T) {
+func TestFirstBabySteps(t *testing.T) {
 	m0 := NewMigration0(&referenceSchema)
 	m0.GenerateSQL()
 	require.True(t, len(m0.SQL) > 0)
 
-	fmt.Println()
-	for _, statement := range m0.SQL {
-		fmt.Printf("%s\n", statement)
+	combinedOutput := strings.Join(m0.SQL, "\n")
+
+	if os.Getenv("DEBUG") != "" {
+		fmt.Printf("\n%s\n\n", combinedOutput)
 	}
+
+	require.Equal(t, expectedBabyStepsSQL, combinedOutput)
 }
+
+const expectedBabyStepsSQL string = `DROP DATABASE IF EXISTS keel;
+CREATE DATABASE keel;
+CREATE TABLE Person (
+  Name TEXT,
+  Age integer,
+);
+CREATE TABLE Vehicle (
+  Make TEXT,
+  PriceNew money,
+);`
 
 var referenceSchema proto.Schema = proto.Schema{
 	Models: []*proto.Model{
 		{
-			Name: "ModelA",
+			Name: "Person",
 			Fields: []*proto.Field{
 				{
 					Name: "Name",
@@ -31,6 +47,19 @@ var referenceSchema proto.Schema = proto.Schema{
 				{
 					Name: "Age",
 					Type: proto.FieldType_FIELD_TYPE_INT,
+				},
+			},
+		},
+		{
+			Name: "Vehicle",
+			Fields: []*proto.Field{
+				{
+					Name: "Make",
+					Type: proto.FieldType_FIELD_TYPE_STRING,
+				},
+				{
+					Name: "PriceNew",
+					Type: proto.FieldType_FIELD_TYPE_CURRENCY,
 				},
 			},
 		},
