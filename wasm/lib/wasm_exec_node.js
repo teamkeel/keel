d003d68722ea32ea400bcb2e690cda4b6e994599
Adam Bull <adam@keel.xyz>
2022-06-29 17:40:15 +0100

    chore: remove commented code

--- wasm/lib/wasm_exec_node.js
+++ wasm/lib/wasm_exec_node.js
@@ -24,21 +24,3 @@ globalThis.crypto = {
 };
 
 require("./wasm_exec");
-
-// const go = new Go();
-// go.argv = process.argv.slice(2);
-// go.env = Object.assign({ TMPDIR: require("os").tmpdir() }, process.env);
-// go.exit = process.exit;
-// WebAssembly.instantiate(fs.readFileSync(process.argv[2]), go.importObject).then((result) => {
-// 	process.on("exit", (code) => { // Node.js exits if no event handler is pending
-// 		if (code === 0 && !go.exited) {
-// 			// deadlock, make Go print error and stack traces
-// 			go._pendingEvent = { id: 0 };
-// 			go._resume();
-// 		}
-// 	});
-// 	return go.run(result.instance);
-// }).catch((err) => {
-// 	console.error(err);
-// 	process.exit(1);
-// });
