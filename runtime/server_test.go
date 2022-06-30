6f1250fc64d32f234f6d626d84ae4aff1dafacd7
peterhoward42 <peter@keel.xyz>
2022-06-30 20:33:36 +0100

    merge in main

--- runtime/server_test.go
+++ runtime/server_test.go
@@ -16,7 +16,7 @@ import (
 
 func TestJustToDriveInitialCodingAndCompiling(t *testing.T) {
 
-	schemaDir := filepath.Join(".", "testdata", "get-simplest")
+	schemaDir := filepath.Join(".", "testdata", "get-simplest-happy")
 	s2m := schema.Builder{}
 	protoSchema, err := s2m.MakeFromDirectory(schemaDir)
 	require.NoError(t, err)
