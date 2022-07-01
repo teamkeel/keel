f60c41f8b89e55444f10a7c6d10d8aefe11ab536
peterhoward42 <peterhoward42@gmail.com>
2022-07-01 12:02:24 +0100

    chore: table tests for runtime/gql package (#119)
    * Make the tests for the runtime/gql package work from expected files
    
    * merge in main
    
    Co-authored-by: peterhoward42 <peter@keel.xyz>

--- runtime/gql/api_test.go
+++ runtime/gql/api_test.go
@@ -2,6 +2,9 @@ package gql
 
 import (
 	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -9,48 +12,84 @@ import (
 	"github.com/teamkeel/keel/schema"
 )
 
-func TestHandlersHappyPath(t *testing.T) {
-	schemaDir := filepath.Join("..", "testdata", "get-simplest")
+// TestHandlerSuite is a table-driven test suite for the Handler type's behaviour.
+// The "table" is implied by what it finds in the ../testdata directory.
+// That directory has a sub directory for each test case.
+// Each such directory should contain:
+//	o  The keel schema to use
+//  o  The graphql request to send
+//  o  Either the expected returned data, or the expected returned errors.
+func TestHandlersSuite(t *testing.T) {
+	testCasesParent := "../testdata"
+	subDirs, err := ioutil.ReadDir(testCasesParent)
+	require.NoError(t, err)
+
+	for _, dir := range subDirs {
+		if !dir.IsDir() {
+			continue
+		}
+		dirPath := filepath.Join(testCasesParent, dir.Name())
+		t.Run(dir.Name(), func(t *testing.T) {
+			runTestCase(t, dirPath)
+		})
+	}
+}
+
+func runTestCase(t *testing.T, dirPath string) {
 	s2m := schema.Builder{}
-	protoSchema, err := s2m.MakeFromDirectory(schemaDir)
+	protoSchema, err := s2m.MakeFromDirectory(dirPath)
 	require.NoError(t, err)
 	protoJSON, err := json.Marshal(protoSchema)
 	require.NoError(t, err)
 
-	// We'll use the highest level wrapper constructor (NewHandlersFromJSON) - because this
-	// exercises the stack of lower level constructors under the hood.
 	handlers, err := NewHandlersFromJSON(string(protoJSON))
 	require.NoError(t, err)
 	chosenHandler, ok := handlers["Web"]
 	require.True(t, ok)
-	result := chosenHandler.Handle(exampleQuery)
-	require.Equal(t, 0, len(result.Errors))
-	require.Equal(t, expected, result.Data)
+
+	// Ask the handler to respond to the query.
+	request := fileContents(t, filepath.Join(dirPath, requestFile))
+	result := chosenHandler.Handle(string(request))
+
+	if expectingHappyPath(t, dirPath) {
+		expectedData := fileContents(t, filepath.Join(dirPath, expectedDataFile))
+		actualData, err := json.MarshalIndent(result.Data, "", "  ")
+		require.NoError(t, err)
+		require.JSONEq(t, string(expectedData), string(actualData))
+	} else {
+		expectedErrors := fileContents(t, filepath.Join(dirPath, expectedErrorsFile))
+		actualErrors, err := json.MarshalIndent(result.Errors, "", "  ")
+		if os.Getenv("DEBUG") != "" {
+			t.Logf("Actual error json is: \n%s\n", actualErrors)
+		}
+		require.NoError(t, err)
+		require.JSONEq(t, string(expectedErrors), string(actualErrors))
+	}
 }
 
-func TestHandlersErrorPath(t *testing.T) {
-	schemaDir := filepath.Join("..", "testdata", "get-simplest")
-	s2m := schema.Builder{}
-	protoSchema, err := s2m.MakeFromDirectory(schemaDir)
-	require.NoError(t, err)
-	protoJSON, err := json.Marshal(protoSchema)
-	require.NoError(t, err)
-
-	handlers, err := NewHandlersFromJSON(string(protoJSON))
-	require.NoError(t, err)
-	chosenHandler, ok := handlers["Web"]
-	require.True(t, ok)
-	result := chosenHandler.Handle(malformedQuery)
-	require.Equal(t, 1, len(result.Errors))
-
-	errorMsg := result.Errors[0].Message
-	require.Equal(t, `Unknown argument "nosuchfield" on field "getAuthor" of type "Query".`, errorMsg)
+func expectingHappyPath(t *testing.T, dir string) bool {
+	return fileExists(t, filepath.Join(dir, expectedDataFile))
 }
 
-const exampleQuery string = `{ getAuthor(name: "fred") { name } }`
-const malformedQuery string = `{ getAuthor(nosuchfield: "fred") { name } }`
+func fileExists(t *testing.T, filePath string) bool {
+	_, err := os.Stat(filePath)
+	if errors.Is(err, os.ErrNotExist) {
+		return false
+	}
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	return true
+}
 
-const expectedKey string = `getAuthor`
+func fileContents(t *testing.T, filePath string) []byte {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		t.Fatalf("cannot read this file: %s, error is: %v", filePath, err)
+	}
+	return data
+}
 
-var expectedData map[string]any = map[string]any{"name": "Harriet"}
-var expected map[string]any = map[string]any{expectedKey: expectedData}
+const expectedDataFile string = "response.json"
+const expectedErrorsFile string = "errors.json"
+const requestFile string = "request.gql"
