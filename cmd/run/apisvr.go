f756c628853c17cb254028891411d271b9785574
peterhoward42 <peter@keel.xyz>
2022-07-06 09:22:08 +0100

    do API server stop/start lifecycle properly

--- cmd/run/apisvr.go
+++ cmd/run/apisvr.go
@@ -1,29 +1,36 @@
 package run
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/teamkeel/keel/runtime"
 )
 
-// startAPIServerAsync first creates an HTTP server that serves the GraphQL API(s), implied
-// by the given (JSON serialized) proto.Schema. Then it launches a go routine in which
-// to tell it to ListenAndServe.
-func startAPIServerAsync(schemaJSON string) error {
-	svr, err := runtime.NewServer(schemaJSON)
+// restartAPIServer stops and then restarts the GraphQL API server referenced by
+// the handler's apiServer field. It is safe to call it when the server has not
+// been started yet.
+func (h *SchemaChangedHandler) retartAPIServer(schemaJSON string) (err error) {
+	if h.apiServer != nil {
+		err := h.apiServer.Shutdown(context.Background())
+		if err != nil {
+			return err
+		}
+	}
+
+	h.apiServer, err = runtime.NewServer(schemaJSON)
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("Your GraphQL server has been restarted. Serving your APIs at localhost:8080/graphql/<api-name>\n")
-
 	// todo put in note about goroutine housekeeping
 	go func() {
-		err := svr.ListenAndServe()
+		err := h.apiServer.ListenAndServe()
 		if err != nil {
 			fmt.Printf("server ListenAndServe terminated with this error: %v", err)
 		}
 	}()
 
+	fmt.Printf("Your GraphQL server has been restarted - serving your APIs at localhost:8080/graphql/<api-name>\n")
 	return nil
 }
