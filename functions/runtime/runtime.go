4983e03c8ec9d5caa7702ec3c0e1a8686b12325c
Adam Bull <adam@keel.xyz>
2022-07-25 13:33:48 +0100

    chore: fix tests

--- functions/runtime/runtime.go
+++ functions/runtime/runtime.go
@@ -22,14 +22,6 @@ type Runtime struct {
 	generator  codegen.CodeGenerator
 }
 
-var DEV_DEPENDENCIES = map[string]string{
-	"@types/node": "^18.0.6",
-	"typescript":  "^4.7.4",
-}
-
-// We don't require any dependencies at the minute
-var DEPENDENCIES = map[string]string{}
-
 var SCHEMA_FILE = "schema.keel"
 var DEV_DIRECTORY = ".keel"
 var FUNCTIONS_DIRECTORY = "functions"
@@ -158,24 +150,6 @@ func (r *Runtime) RunServer(port int, onBoot func(process *os.Process)) error {
 	}
 
 	onBoot(cmd.Process)
-
-	return nil
-}
-
-func (r *Runtime) BootstrapPackageJson() error {
-	path := filepath.Join(r.WorkingDir, "package.json")
-
-	packageJson, err := NewPackageJson(path)
-
-	if err != nil {
-		return err
-	}
-
-	err = packageJson.Inject(DEV_DEPENDENCIES, DEPENDENCIES)
-
-	if err != nil {
-		return err
-	}
 
 	return nil
 }
