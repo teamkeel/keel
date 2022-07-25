926a3157ddf31c055e4f09d4368b0eabc12804f2
peterhoward42 <peterhoward42@gmail.com>
2022-07-25 16:37:37 +0100

    chore: tweek signature of Create() (#173)
    Co-authored-by: peterhoward42 <peter@keel.xyz>

--- runtime/runtimectx/ctx.go
+++ runtime/runtimectx/ctx.go
@@ -2,39 +2,26 @@ package runtimectx
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
-	"github.com/teamkeel/keel/proto"
 	"gorm.io/gorm"
 )
 
-func GetDB(ctx context.Context) *gorm.DB {
+func GetDB(ctx context.Context) (*gorm.DB, error) {
 	v := ctx.Value(dbKey)
 	if v == nil {
-		// todo - if this remains connected after some more work on Create action, then
-		// convert it to returning an error
-		panic(fmt.Sprintf("Context does not have key: %s", dbKey))
+		return nil, fmt.Errorf("context does not have a :%s key", dbKey)
 	}
-	return v.(*gorm.DB)
-}
-
-func GetSchema(ctx context.Context) *proto.Schema {
-	v := ctx.Value(schemaKey)
-	if v == nil {
-		// todo - if this remains connected after some more work on Create action, then
-		// convert it to returning an error
-		panic(fmt.Sprintf("Context does not have key: %s", schemaKey))
+	db, ok := v.(*gorm.DB)
+	if !ok {
+		return nil, errors.New("database in the context has wrong value type")
 	}
-	return v.(*proto.Schema)
+	return db, nil
 }
 
 func WithDB(parent context.Context, db *gorm.DB) context.Context {
 	return context.WithValue(parent, dbKey, db)
 }
 
-func WithSchema(parent context.Context, schema *proto.Schema) context.Context {
-	return context.WithValue(parent, schemaKey, schema)
-}
-
 const dbKey string = "database"
-const schemaKey = "schema"
