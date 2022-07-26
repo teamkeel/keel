1dadd2542806c409dec5b5ae7d7d8478f1fef382
peterhoward42 <peter@keel.xyz>
2022-07-26 22:08:57 +0100

    chore: provide a getValueAtPath() function

--- runtime/runtime_test.go
+++ runtime/runtime_test.go
@@ -19,19 +19,105 @@ import (
 	"gorm.io/gorm"
 )
 
+func TestRuntime(t *testing.T) {
+	// We connect to the "main" database here only so we can create a new database
+	// for each sub-test
+	mainDB, err := gorm.Open(
+		postgres.Open(fmt.Sprintf(dbConnString, "keel")),
+		&gorm.Config{})
+	require.NoError(t, err)
+
+	for _, tCase := range testCases {
+
+		t.Run(tCase.name, func(t *testing.T) {
+
+			// Make a database name for this test
+			re := regexp.MustCompile(`[^\w]`)
+			dbName := strings.ToLower(re.ReplaceAllString(tCase.name, ""))
+
+			// Drop the database if it already exists. The normal dropping of it at the end of the
+			// test case is bypassed if you quit a debug run of the test in VS Code.
+			require.NoError(t, mainDB.Exec("DROP DATABASE if exists "+dbName).Error)
+
+			// Create the database and drop at the end of the test
+			err = mainDB.Exec("CREATE DATABASE " + dbName).Error
+			require.NoError(t, err)
+			defer func() {
+				require.NoError(t, mainDB.Exec("DROP DATABASE "+dbName).Error)
+			}()
+
+			// Connect to the newly created test database and close connection
+			// at the end of the test. We need to explicitly close the connection
+			// so the mainDB connection can drop the database.
+			testDB, err := gorm.Open(
+				postgres.Open(fmt.Sprintf(dbConnString, dbName)),
+				&gorm.Config{})
+			require.NoError(t, err)
+			defer func() {
+				conn, err := testDB.DB()
+				require.NoError(t, err)
+				conn.Close()
+			}()
+
+			// Migrate the database to this test case's schema.
+			schema := protoSchema(t, tCase.keelSchema)
+			m := migrations.New(schema, nil)
+			require.NoError(t, m.Apply(testDB))
+
+			// Construct the runtime API Handler.
+			handler := NewHandler(schema)
+
+			// Assemble the query to send from the test case data.
+			reqBody := queryAsJSONPayload(t, tCase.gqlOperation, tCase.variables)
+			request := Request{
+				Context: runtimectx.NewContext(testDB),
+				URL: url.URL{
+					Path: "/Test",
+				},
+				Body: []byte(reqBody),
+			}
+
+			// Call the handler, and capture the response.
+			response, err := handler(&request)
+			respString := string(response.Body)
+			_ = respString
+			require.NoError(t, err)
+
+			_ = response
+
+			// Do some assertions
+		})
+	}
+}
+
 const dbConnString = "host=localhost port=8001 user=postgres password=postgres dbname=%s sslmode=disable"
 
-// keel schema + build + migrations
-// database setup (optional)
-// graphql operation (query or mutation)
-// graphql operation variables (JSON, optional)
-// assert against the response data (optional)
-// assert against the response errors (optional)
-// assert against the database
+func protoSchema(t *testing.T, keelSchema string) *proto.Schema {
+	builder := &schema.Builder{}
+	schema, err := builder.MakeFromInputs(&reader.Inputs{
+		SchemaFiles: []reader.SchemaFile{
+			{
+				Contents: keelSchema,
+			},
+		},
+	})
+	require.NoError(t, err)
+	return schema
+}
+
+func queryAsJSONPayload(t *testing.T, mutationString string, vars map[string]any) (asJSON string) {
+	d := map[string]any{
+		"query":     mutationString,
+		"variables": vars,
+	}
+	b, err := json.Marshal(d)
+	require.NoError(t, err)
+	return string(b)
+}
 
 type testCase struct {
 	name           string
-	schema         string
+	keelSchema     string
 	databaseSetup  func(t *testing.T, db *gorm.DB)
 	gqlOperation   string
 	variables      map[string]any
@@ -43,7 +129,7 @@ type testCase struct {
 var testCases = []testCase{
 	{
 		name: "create_operation",
-		schema: `
+		keelSchema: `
 			model Person {
 				fields {
 					name Text
@@ -77,113 +163,11 @@ var testCases = []testCase{
 		assertErrors: func(t *testing.T, errors []gqlerrors.FormattedError) {
 		},
 		assertDatabase: func(t *testing.T, db *gorm.DB, data map[string]any) {
-			id := getValueAtPath(data, "createPerson.id")
-
+			id := getValueAtPath(t, data, "createPerson.id")
 			var name string
 			err := db.Table("person").Where("id = ?", id).Pluck("name", &name).Error
 			require.NoError(t, err)
 			require.Equal(t, "Fred", name)
 		},
 	},
-}
-
-func assertValueAtPath(t *testing.T, d map[string]any, path string, v any) {
-	require.Equal(t, v, getValueAtPath(d, "createPerson.name"))
-}
-
-func getValueAtPath(d map[string]any, path string) any {
-	return ""
-}
-
-func TestRuntime(t *testing.T) {
-	// We connect to the "main" database here only so we can create a new database
-	// for each sub-test
-	mainDB, err := gorm.Open(
-		postgres.Open(fmt.Sprintf(dbConnString, "keel")),
-		&gorm.Config{})
-	require.NoError(t, err)
-
-	for _, tCase := range testCases {
-
-		_ = tCase
-
-		t.Run(tCase.name, func(t *testing.T) {
-
-			// Make a database name for this test
-			re := regexp.MustCompile(`[^\w]`)
-			dbName := strings.ToLower(re.ReplaceAllString(tCase.name, ""))
-
-			// Drop the database if it already exists. The normal dropping of it at the end of the
-			// test case is bypassed if you quit a debug run of the test in VS Code.
-			require.NoError(t, mainDB.Exec("DROP DATABASE if exists "+dbName).Error)
-
-			// Create the database and drop at the end of the test
-			err = mainDB.Exec("CREATE DATABASE " + dbName).Error
-			require.NoError(t, err)
-			defer func() {
-				require.NoError(t, mainDB.Exec("DROP DATABASE "+dbName).Error)
-			}()
-
-			// Connect to the newly created test database and close connection
-			// at the end of the test. We need to explicitly close the connection
-			// so the mainDB connection can drop the database.
-			testDB, err := gorm.Open(
-				postgres.Open(fmt.Sprintf(dbConnString, dbName)),
-				&gorm.Config{})
-			require.NoError(t, err)
-			defer func() {
-				conn, err := testDB.DB()
-				require.NoError(t, err)
-				conn.Close()
-			}()
-
-			schema := protoSchema(t, tCase.schema)
-
-			// Migrate the database to this schema, in readiness for the Create Action.
-			m := migrations.New(schema, nil)
-			require.NoError(t, m.Apply(testDB))
-
-			handler := NewHandler(schema)
-			reqBody := queryAsJSONPayload(t, tCase.gqlOperation, tCase.variables)
-			request := Request{
-				Context: runtimectx.NewContext(testDB),
-				URL: url.URL{
-					Path: "/Test",
-				},
-				Body: []byte(reqBody),
-			}
-
-			response, err := handler(&request)
-			respString := string(response.Body)
-			_ = respString
-			require.NoError(t, err)
-
-			_ = response
-
-			// Do some assertions
-		})
-	}
-}
-
-func protoSchema(t *testing.T, s string) *proto.Schema {
-	builder := &schema.Builder{}
-	schema, err := builder.MakeFromInputs(&reader.Inputs{
-		SchemaFiles: []reader.SchemaFile{
-			{
-				Contents: s,
-			},
-		},
-	})
-	require.NoError(t, err)
-	return schema
-}
-
-func queryAsJSONPayload(t *testing.T, mutationString string, vars map[string]any) (asJSON string) {
-	d := map[string]any{
-		"query":     mutationString,
-		"variables": vars,
-	}
-	b, err := json.Marshal(d)
-	require.NoError(t, err)
-	return string(b)
 }
