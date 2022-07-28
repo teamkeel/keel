ba4174534545fd1b796c83e656ece52da939869d
Adam Bull <adam@keel.xyz>
2022-07-28 15:33:26 +0100

    chore: process single file only

--- functions/runtime/src/analyse.ts
+++ functions/runtime/src/analyse.ts
@@ -5,8 +5,5 @@ export interface AnalyseOptions {
 }
 
 export default async (opts: AnalyseOptions) => {
-  // parseFiles may take an array of paths, but it actually only works
-  // for a single path
-  // Passing in multiple file paths with default exports within each doesnt work
   return parseFiles([opts.path], { collectParameters: true })
 }
--- functions/runtime/src/cli.ts
+++ functions/runtime/src/cli.ts
@@ -1,32 +1,19 @@
 import { program } from 'commander'
 import util from 'util'
-import path from 'path';
-
-const glob = util.promisify(require('glob'))
-
 import analyse from './analyse'
 
 program
   .version('0.0.1')
   .description('Statically analyses a custom function directory')
-  .argument('<string>', 'Path to directory')
+  .argument('<string>', 'Path to file')
   .option('-d, --debug', 'Debug mode')
-  .action(async (dir, opts) => {
-    const pattern = path.join(dir, 'functions', '*.ts')
-    const tsFiles: string[] = await glob(pattern)
-
-    let json = {}
-
-    for await (const result of tsFiles.map(
-      async (p) => ({ [p]: await analyse({ path: p }) }))
-    ) {
-      json = { ...json, ...result }
-    }
+  .action(async (path, opts) => {
+    const result = await analyse({ path }) 
 
     if (opts.debug) {
-      console.log(util.inspect(json, { showHidden: false, colors: true, depth: 5 }))
+      console.log(util.inspect(result, { showHidden: false, colors: true, depth: 5 }))
     } else {
-      console.log('%j', json)
+      console.log('%j', result)
     }
   })
 
