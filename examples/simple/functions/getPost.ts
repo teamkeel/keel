b53b4650af7cd19d1b2d1e384c8319f2c04b9f18
Adam Bull <adam@keel.xyz>
2022-08-09 11:13:22 +0100

    feat: work on database connection setup

---
+++ examples/simple/functions/getPost.ts
@@ -0,0 +1,12 @@
+import { GetPost } from '@teamkeel/sdk'
+
+export default GetPost(async (inputs, api) => {
+  // Build something awesome
+
+  return {
+    title: 'djjd',
+    id: '8383',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }
+})
