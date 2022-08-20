70b898ef080d2ebfb6cc199ea130e1e148859011
Adam Bull <adam@keel.xyz>
2022-08-20 12:06:25 +0100

    build: use proper query api invocation (#276)

--- integration/testdata/simple_database_test/example.test.ts
+++ integration/testdata/simple_database_test/example.test.ts
@@ -1,14 +1,12 @@
 import { test, expect, Post } from '@teamkeel/testing'
 
 test('it passes', async () => {
-  const api = await Post
-  const p = await api.create({ title: 'apple' })
+  const p = await Post.create({ title: 'apple' })
   expect.equal(p.title, 'apple')
 })
 
 test('it fails', async () => {
-  const api = await Post
-  const p = await api.create({ title: 'apple' })
+  const p = await Post.create({ title: 'apple' })
   expect.equal(p.title, 'orange')
 })
 
