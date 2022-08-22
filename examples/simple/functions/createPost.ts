87df5f9e04eb9946360e3e32a9a7445285cf57a5
Adam Bull <adam@keel.xyz>
2022-08-22 12:46:46 +0100

    chore: fix simple example

--- examples/simple/functions/createPost.ts
+++ examples/simple/functions/createPost.ts
@@ -1,13 +1,5 @@
 import { CreatePost } from '@teamkeel/sdk'
 
 export default CreatePost(async (inputs, api) => {
-
-  console.log(api)
-  const { Post } = api.models;
-
-  const res = await Post.create(inputs)
-
-  console.log(res)
-
-  return res
+  return await api.models.post.create(inputs)
 })
