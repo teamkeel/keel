f56386453e93573c0e4b57d1222d033c19177491
Adam Bull <adam@keel.xyz>
2022-08-24 11:24:14 +0100

    test: more tests

--- integration/testdata/simple_database_test/example.test.ts
+++ integration/testdata/simple_database_test/example.test.ts
@@ -5,6 +5,14 @@ test('create', async () => {
   expect.equal(p.title, 'apple')
 })
 
+test('update', async () => {
+  const post = await Post.create({ title: 'star wars' })
+
+  const updatedPost = await Post.update(post.id, { title: 'star wars sucks!' })
+
+  expect.equal(updatedPost.title, 'star wars sucks!')
+})
+
 test('findOne', async () => {
   await Post.create({ title: 'apple' })
   await Post.create({ title: 'granny apple' })
@@ -29,4 +37,33 @@ test('findMany', async () => {
   }).all()
 
   expect.equal(allFruit.length, 2)
+})
+
+test('chained conditions', async () => {
+  await Post.create({ title: 'melon' })
+  await Post.create({ title: 'kiwi' })
+
+  const matches = await Post.where({
+    title: 'melon'
+  }).orWhere({
+    title: 'kiwi'
+  }).all()
+
+  expect.equal(matches.length, 2)
+})
+
+test('order', async () => {
+  await Post.create({ title: 'abc' })
+  await Post.create({ title: 'bcd' })
+
+  const orderedAlphabetically = await Post.where({
+    title: {
+      contains: 'bc'
+    }
+  }).order({
+    title: 'desc'
+  }).all()
+
+  expect.equal(orderedAlphabetically.length, 2)
+  expect.equal(orderedAlphabetically[0].title, 'abc')
 })
