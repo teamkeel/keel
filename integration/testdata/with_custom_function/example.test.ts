fe74d15f0b4a3f4d9d4f4bbd3fefb59811488ea8
Adam Bull <adam@keel.xyz>
2022-08-24 12:56:53 +0100

    test: add test for custom function action execution (#305)
    * test: add test for custom function action execution
    
    * test: update custom function to use correct api

--- integration/testdata/with_custom_function/example.test.ts
+++ integration/testdata/with_custom_function/example.test.ts
@@ -1,11 +1,14 @@
-import { test, expect, Actions } from '@teamkeel/testing'
+import { test, expect, Actions, Person } from '@teamkeel/testing'
 
-test('it passes', async () => {
+test('creating a person', async () => {
   const result = await Actions.createPerson({ title: 'foo' })
   expect.equal(result.title, 'foo')
 })
 
-test('it fails', async () => {
-  const result = await Actions.createPerson({ title: 'bar' })
-  expect.equal(result.title, 'foo')
+test('fetching a person by id', async () => {
+  const person = await Person.create({ title: 'bar' })
+  const res = await Actions.getPerson({ id: person.id })
+
+  expect.equal(res.id, person.id)
+  expect.equal(res.title, person.title)
 })
