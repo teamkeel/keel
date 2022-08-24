4f9b18f2fe201e03703d2878e33cdb1909bb2a6e
Adam Bull <adam@keel.xyz>
2022-08-24 14:41:27 +0100

    fix: fixes issues with testing + runtime packages

--- functions/runtime/src/cli.ts
+++ functions/runtime/src/cli.ts
@@ -1,20 +1,22 @@
-import { program } from 'commander'
-import util from 'util'
-import analyse from './analyse'
+import { program } from "commander";
+import util from "util";
+import analyse from "./analyse";
 
 program
-  .version('0.0.1')
-  .description('Statically analyses a custom function file')
-  .argument('<string>', 'Path to file')
-  .option('-d, --debug', 'Debug mode')
+  .version("0.0.1")
+  .description("Statically analyses a custom function file")
+  .argument("<string>", "Path to file")
+  .option("-d, --debug", "Debug mode")
   .action(async (path, opts) => {
-    const result = await analyse({ path }) 
+    const result = await analyse({ path });
 
     if (opts.debug) {
-      console.log(util.inspect(result, { showHidden: false, colors: true, depth: null }))
+      console.log(
+        util.inspect(result, { showHidden: false, colors: true, depth: null })
+      );
     } else {
-      console.log('%j', result)
+      console.log("%j", result);
     }
-  })
+  });
 
-program.parse()
+program.parse();
