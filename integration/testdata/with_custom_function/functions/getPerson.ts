8d12c352cc14dbc44fd884f12c7600f2cd7e5e26
Adam Bull <adam@keel.xyz>
2022-08-26 13:46:12 +0100

    chore: add more tests

--- integration/testdata/with_custom_function/functions/getPerson.ts
+++ integration/testdata/with_custom_function/functions/getPerson.ts
@@ -1,5 +1,5 @@
 import { GetPerson } from '@teamkeel/sdk'
 
 export default GetPerson(async (inputs, api) => {
-  return await api.models.person.findOne({ id: inputs.id })
+  return await api.models.person.findOne(inputs)
 })
