2e76e7754e14ef09526ada78d395320769786d99
Adam Bull <adam@keel.xyz>
2022-08-26 11:17:29 +0100

    fix: use lodash isEqual to check equality between expected vs actual

--- testing/package/src/expect.ts
+++ testing/package/src/expect.ts
@@ -1,8 +1,12 @@
+import isEqual from 'lodash.isequal'
 import { AssertionFailure } from "./errors";
 
 const expect = {
   equal: (actual: any, expected: any): void => {
-    if (actual !== expected) {
+    // Lodash's isEqual checks equality for many different types:
+    // arrays, booleans, dates, errors, maps, numbers, objects, regexes, sets, strings, symbols
+    // Ref: https://lodash.com/docs/4.17.15#isEqual
+    if (!isEqual(actual, expected)) {
       throw new AssertionFailure(actual, expected);
     }
   },
