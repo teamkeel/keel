90e17d4110dcddfb93407833074053f7b03bc639
Adam Bull <adam@keel.xyz>
2022-09-26 16:30:15 +0100

    feat: adds support for built-in delete action

--- integration/testdata/built_in_actions/main.test.ts
+++ integration/testdata/built_in_actions/main.test.ts
@@ -2,7 +2,7 @@ import { test, expect, Actions, Post } from '@teamkeel/testing'
 
 test('create action', async () => {
   // todo: /Users/adambull/dev/keel/runtime/actions/create.go:46 ERROR: null value in column "title" of relation "post" violates not-null constraint (SQLSTATE 23502)
-  const { object: createdPost } = await Actions.createPost({ title: 'foo' })
+  const { object: createdPost } = await Actions.createPost({ title: 'foo', subTitle: 'abc' })
   expect.equal(createdPost.title, 'foo')
 })
 
@@ -14,7 +14,7 @@ test('create action (unrecognised fields)', async () => {
 })
 
 test('get action', async () => {
-  const { object: post } = await Actions.createPost({ title: 'foo' })
+  const { object: post } = await Actions.createPost({ title: 'foo', subTitle: 'bcd' })
   const { object: fetchedPost } = await Actions.getPost({ id: post.id })
 
   expect.equal(fetchedPost.id, post.id)
@@ -22,7 +22,7 @@ test('get action', async () => {
 
 // This test verifies that you can't fetch by a field not specified in action inputs
 test('get action (non unique)', async () => {
-  const { object: post } = await Actions.createPost({ title: 'foo' })
+  const { object: post } = await Actions.createPost({ title: 'foo', subTitle: 'cbd' })
   const { object: fetchedPost } = await Actions.getPost({ title: post.title })
 
   // todo: until we return errors for 404s, we want to assert
@@ -31,8 +31,8 @@ test('get action (non unique)', async () => {
 })
 
 test('list action - equals', async () => {
-  await Post.create({ title: 'apple' })
-  await Post.create({ title: 'apple' })
+  await Post.create({ title: 'apple', subTitle: 'def' })
+  await Post.create({ title: 'apple', subTitle: 'efg' })
 
   const { collection } = await Actions.listPosts({ title: { equals: 'apple' } })
 
@@ -40,8 +40,8 @@ test('list action - equals', async () => {
 })
 
 test('list action - contains', async () => {
-  await Post.create({ title: 'banan' })
-  await Post.create({ title: 'banana' })
+  await Post.create({ title: 'banan', subTitle: 'fgh' })
+  await Post.create({ title: 'banana', subTitle: 'ghi' })
 
   const { collection } = await Actions.listPosts({ title: { contains: 'ana' } })
 
@@ -49,8 +49,8 @@ test('list action - contains', async () => {
 })
 
 test('list action - startsWith', async () => {
-  await Post.create({ title: 'adam' })
-  await Post.create({ title: 'adamant' })
+  await Post.create({ title: 'adam', subTitle: 'hij' })
+  await Post.create({ title: 'adamant', subTitle: 'ijk' })
 
   const { collection } = await Actions.listPosts({ title: { startsWith: 'adam' } })
 
@@ -58,8 +58,8 @@ test('list action - startsWith', async () => {
 })
 
 test('list action - endsWith', async () => {
-  await Post.create({ title: 'star wars' })
-  await Post.create({ title: 'a post about star wars' })
+  await Post.create({ title: 'star wars', subTitle: 'jkl' })
+  await Post.create({ title: 'a post about star wars', subTitle: 'klm' })
 
   const { collection } = await Actions.listPosts({ title: { endsWith: 'star wars' } })
 
@@ -67,10 +67,26 @@ test('list action - endsWith', async () => {
 })
 
 test('list action - oneOf', async () => {
-  await Post.create({ title: 'pear' })
-  await Post.create({ title: 'mango' })
+  await Post.create({ title: 'pear', subTitle: 'lmn' })
+  await Post.create({ title: 'mango', subTitle: 'mno' })
 
   const { collection } = await Actions.listPosts({ title: { oneOf: ['pear', 'mango'] } })
 
   expect.equal(collection.length, 2)
+})
+
+test('delete action', async () => {
+  const { object: post } = await Post.create({ title: 'pear', subTitle: 'nop' })
+
+  const { success } = await Actions.deletePost({ id: post.id })
+
+  expect.equal(success, true)
+})
+
+test('delete action (other unique field)', async () => {
+  const { object: post } = await Post.create({ title: 'pear', subTitle: 'nop' })
+
+  const { success } = await Actions.deletePostBySubTitle({ subTitle: post.subTitle })
+
+  expect.equal(success, true)
 })
