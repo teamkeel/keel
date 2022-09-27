e9db86f1ef2c3e8cf4cf36550761587b0c8fff23
Adam Bull <adam@keel.xyz>
2022-09-27 16:58:04 +0100

    feat: reset db after each test run (#387)
    * feat: reset db after each test run
    
    * chore: prettier

--- testing/package/src/reporter.test.ts
+++ testing/package/src/reporter.test.ts
@@ -2,11 +2,11 @@ import { TestResultData } from "output";
 import Reporter from "./reporter";
 
 describe("reporter", () => {
-  describe('report', () => {
+  describe("report", () => {
     beforeEach(() => {
       fetchMock.resetMocks();
     });
-  
+
     it("sends a request to the correct url", async () => {
       fetchMock.mockResponseOnce(
         JSON.stringify({
@@ -15,23 +15,23 @@ describe("reporter", () => {
           },
         })
       );
-  
+
       const reporter = new Reporter({ port: 123, host: "localhost" });
-  
+
       const testResults = [
         {
           testName: "a passing test",
           status: "pass",
         },
       ] as TestResultData[];
-  
+
       await reporter.report(testResults);
-  
+
       expect(fetchMock.mock.calls[0][1]).toEqual({
         method: "POST",
         body: '[{"testName":"a passing test","status":"pass"}]',
       });
       expect(fetchMock.mock.calls[0][0]).toEqual("http://localhost:123/report");
     });
-  })
+  });
 });
