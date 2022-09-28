92621b29c3d2dc5c8faee807eb95c107903fb121
Adam Bull <adam@keel.xyz>
2022-09-28 11:31:00 +0100

    chore: undo test skip

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -4,14 +4,12 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io/fs"
 	"io/ioutil"
 	"path/filepath"
 	gotest "testing"
 
 	"github.com/alexflint/go-restructure/regex"
 	"github.com/nsf/jsondiff"
-	"github.com/samber/lo"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/teamkeel/keel/nodedeps"
@@ -29,18 +27,10 @@ type Expected struct {
 }
 
 func TestIntegration(t *gotest.T) {
-	t.Skip()
 	entries, err := ioutil.ReadDir("./testdata")
 	require.NoError(t, err)
 
-	var skip string = "" // Name of the test directory you want to skip, or ""
-	var only = ""        // Name of the test directory you want isolated and alone, or ""
-
-	filteredEntries := lo.Filter(entries, func(entry fs.FileInfo, _ int) bool {
-		return (skip != "" && entry.Name() != skip) || (only != "" && entry.Name() == only)
-	})
-
-	for _, e := range filteredEntries {
+	for _, e := range entries {
 		t.Run(e.Name(), func(t *gotest.T) {
 			workingDir, err := testhelpers.WithTmpDir(filepath.Join("./testdata", e.Name()))
 
