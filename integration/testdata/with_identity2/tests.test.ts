f2eb038df71ff21a37b3f282fe62b4ca8a5784c8
Dave New <sanodn@gmail.com>
2022-10-02 10:10:59 +0200

    chore: withIdentity(model)

---
+++ integration/testdata/with_identity2/tests.test.ts
@@ -0,0 +1,65 @@
+import { test, expect, actions, Post, Identity } from '@teamkeel/testing'
+
+test('authorization successful', async () => {
+  const { identityId } = await actions.authenticate({ 
+    createIfNotExists: true, 
+    email: 'user@keel.xyz',
+    password: '1234'})
+
+  const { object: identity } = await Identity.findOne({ id: identityId })
+
+  const { object: post } = await actions
+    .withIdentity(identity!)
+    .createPost({ title: 'temp' });
+
+  const { errors } = await actions
+    .withIdentity(identity!)
+    .getPost({ id: post.id });
+
+  var authorizationFailed = hasAuthorizationError(errors)
+  expect.equal(authorizationFailed, false)
+})
+
+test('authorization failed', async () => {
+  const { identityId: id1 } = await actions.authenticate({ 
+    createIfNotExists: true, 
+    email: 'user@keel.xyz',
+    password: '1234'})
+
+  const { identityId: id2 } = await actions.authenticate({ 
+    createIfNotExists: true, 
+    email: 'user@keel.xyz',
+    password: '1234'})
+
+  const { object: identity1 } = await Identity.findOne({ id: id1 })
+  const { object: identity2 } = await Identity.findOne({ id: id2 })
+
+console.log(identity1)
+console.log(identity1.id)
+
+
+  const { object: post } = await actions
+    .withIdentity(identity1)
+    .createPost({ title: 'temp' });
+
+  const { errors } = await actions
+    .withIdentity(identity2)
+    .getPost({ id: post.id });
+
+  var authorizationFailed = hasAuthorizationError(errors)
+  expect.equal(authorizationFailed, true)
+})
+
+function hasAuthorizationError(errors?): boolean {
+  if (errors == null)
+    return false;
+
+  var hasError = false
+   errors.forEach(function(error) {
+    if(error.message == 'not authorized to access this operation') {
+      hasError = true
+    }
+  });
+  
+  return hasError;
+}
