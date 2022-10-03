e816549d81c7d919efda6c04a503a4bde0bd0098
Adam Bull <adam@keel.xyz>
2022-10-03 16:33:39 +0100

    feat: better js error capturing to reporter

--- testing/package/src/output.ts
+++ testing/package/src/output.ts
@@ -78,7 +78,6 @@ export class TestResult {
       // so we need to serialize their properties
       // See https://stackoverflow.com/questions/18391212/is-it-not-possible-to-stringify-an-error-using-json-stringify
       const { stack, message, name } = obj.err;
-
       return JSON.stringify({
         ...obj,
         err: {
