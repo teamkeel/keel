5073e42f8f0136d1029ae5d64da212e1bc67d8fc
Adam Bull <adam@keel.xyz>
2022-10-16 22:46:47 +0100

    feat: fix incorrect formatting of explicit inputs (#469)

--- schema/format/format.go
+++ schema/format/format.go
@@ -303,16 +303,18 @@ func printOperationInputs(writer *Writer, inputs []*parser.ActionInputNode) {
 		}
 
 		if arg.Label != nil {
-			writer.Write("%s: ", arg.Label.Value)
-		}
+			// explicit input
+			writer.Write("%s: %s", arg.Label.Value, arg.Type.Fragments[0].Fragment)
+		} else {
 
-		// Note: not using arg.Type.ToString() here as we want to try
-		// and fix any casing issues
-		for i, fragment := range arg.Type.Fragments {
-			if i > 0 {
-				writer.Write(".")
+			// Note: not using arg.Type.ToString() here as we want to try
+			// and fix any casing issues
+			for i, fragment := range arg.Type.Fragments {
+				if i > 0 {
+					writer.Write(".")
+				}
+				writer.Write(lowerCamel(fragment.Fragment))
 			}
-			writer.Write(lowerCamel(fragment.Fragment))
 		}
 
 		if arg.Optional {
