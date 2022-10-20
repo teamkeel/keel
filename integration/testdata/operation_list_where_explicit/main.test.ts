538aa269d756ca25689ad1c93fc71cf16bf7d24d
peterhoward42 <peter@keel.xyz>
2022-10-20 14:55:39 +0100

    chore: tests

--- integration/testdata/operation_list_where_explicit/main.test.ts
+++ integration/testdata/operation_list_where_explicit/main.test.ts
@@ -1,13 +1,26 @@
 import { test, expect, actions, Post, logger } from '@teamkeel/testing'
 
-test('startsWith finds correct records', async () => {
+test('List Where filters - using equal operator (string) - filters correctly', async () => {
   await Post.create({ title: 'Fred' })
   await Post.create({ title: 'NotFred' })
 
-  const { collection: response } = await actions.listPostsEqualityString({ 
-      values: { requiredTitle: "Fred" }
+  const { collection: response } = await actions.listPostsEqualString({ 
+      requiredTitle: "Fred" 
     })
 
   expect(response.length).toEqual(1)
   expect(response[0].title).toEqual('Fred')
+})
+
+
+test('List Where filters - using not equal operator (string) - filters correctly', async () => {
+  await Post.create({ title: 'Fred' })
+  await Post.create({ title: 'NotFred' })
+
+  const { collection: response } = await actions.listPostsNotEqualString({ 
+      requiredTitle: "Fred"
+    })
+
+  expect(response.length).toEqual(1)
+  expect(response[0].title).toEqual('NotFred')
 })
