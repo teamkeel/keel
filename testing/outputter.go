f57435da48ab35e6079ed9c0e5454e62ce3541eb
Adam Bull <adam@keel.xyz>
2022-10-28 15:43:36 +0100

    chore: fix test

---
+++ testing/outputter.go
@@ -0,0 +1,15 @@
+package testing
+
+// type Outputter struct {
+// 	results chan []*Event
+// }
+
+// func NewOutputter(ch chan []*Event) *Outputter {
+// 	return &Outputter{
+// 		results: ch,
+// 	}
+// }
+
+// func (o *Outputter) Start() error {
+
+// }
