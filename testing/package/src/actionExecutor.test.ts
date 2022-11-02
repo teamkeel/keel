8addefc81c4f2e43868cc5f1e78433173bb45e61
Adam Bull <adam@keel.xyz>
2022-11-02 13:27:07 +0000

    feat: throw an error if an action returns any errors

--- testing/package/src/actionExecutor.test.ts
+++ testing/package/src/actionExecutor.test.ts
@@ -5,30 +5,63 @@ describe("execute an action", () => {
     fetchMock.resetMocks();
   });
 
-  it("succeeds", async () => {
-    fetchMock.mockResponseOnce(
-      JSON.stringify({
+  describe('happy path', () => {
+    it("succeeds", async () => {
+      fetchMock.mockResponseOnce(
+        JSON.stringify({
+          object: {
+            title: "text",
+          },
+        })
+      );
+      const executor = new ActionExecutor({
+        parentPort: 123,
+        protocol: "http",
+        host: "localhost",
+      });
+  
+      const result = await executor.execute({
+        actionName: "createPost",
+        payload: { title: "a post" },
+      });
+  
+      expect(fetchMock.mock.calls[0][0]).toEqual("http://localhost:123/action");
+      expect(result).toEqual({
         object: {
           title: "text",
         },
-      })
-    );
-    const executor = new ActionExecutor({
-      parentPort: 123,
-      protocol: "http",
-      host: "localhost",
-    });
-
-    const result = await executor.execute({
-      actionName: "createPost",
-      payload: { title: "a post" },
-    });
-
-    expect(fetchMock.mock.calls[0][0]).toEqual("http://localhost:123/action");
-    expect(result).toEqual({
-      object: {
-        title: "text",
-      },
+      });
     });
   });
+
+  describe('when the action has errors in the response', () => {
+    it('throws the errors', async () => {
+      fetchMock.mockResponseOnce(
+        JSON.stringify({
+          object: {
+            title: "text",
+          },
+          errors: [
+            {
+              message: 'first error'
+            },
+            {
+              message: 'second error'
+            }
+          ]
+        })
+      );
+      const executor = new ActionExecutor({
+        parentPort: 123,
+        protocol: "http",
+        host: "localhost",
+      });
+  
+
+      await expect(async() => await executor.execute({
+        actionName: "createPost",
+        payload: { title: "a post" },
+      })).rejects.toThrowError('first error, second error')
+    });
+  })
 });
