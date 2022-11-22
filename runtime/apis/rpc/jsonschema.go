68512ecda020b5ce733cd610f8e81929d06f8791
Jon Bretman <jon.bretman@gmail.com>
2022-11-22 19:43:25 +0000

    build: implement JSON schema validation for all action types

--- runtime/apis/rpc/jsonschema.go
+++ runtime/apis/rpc/jsonschema.go
@@ -3,42 +3,245 @@ package rpc
 import (
 	"context"
 
+	"github.com/samber/lo"
 	"github.com/teamkeel/keel/proto"
 	"github.com/xeipuuv/gojsonschema"
 )
 
 type JSONSchema struct {
-	Type       string                `json:"type,omitempty"`
-	Properties map[string]JSONSchema `json:"properties,omitempty"`
-	Required   []string              `json:"required,omitempty"`
+	Type                 any                   `json:"type"`
+	Format               string                `json:"format,omitempty"`
+	AdditionalProperties bool                  `json:"additionalProperties"`
+	AnyOf                []JSONSchema          `json:"anyOf,omitempty"`
+	Enum                 []string              `json:"enum,omitempty"`
+	Items                *JSONSchema           `json:"items,omitempty"`
+	Properties           map[string]JSONSchema `json:"properties,omitempty"`
+	Required             []string              `json:"required,omitempty"`
 }
 
+// ValidateRequest validates that the input is valid for the given operation and schema.
+// If validation errors are found they will be contained in the returned result. If an error
+// is returned then validation could not be completed, likely to do an invalid JSON schema
+// being created.
 func ValidateRequest(ctx context.Context, schema *proto.Schema, op *proto.Operation, input map[string]any) (*gojsonschema.Result, error) {
 	requestType := jsonSchemaForOperation(ctx, schema, op)
 	return gojsonschema.Validate(gojsonschema.NewGoLoader(requestType), gojsonschema.NewGoLoader(input))
 }
 
 func jsonSchemaForOperation(ctx context.Context, schema *proto.Schema, op *proto.Operation) JSONSchema {
-	s := JSONSchema{
+	root := JSONSchema{
+		Type:       "object",
 		Properties: map[string]JSONSchema{},
 	}
 
-	for _, input := range op.Inputs {
-		prop := JSONSchema{}
+	where := JSONSchema{
+		Type:       "object",
+		Properties: map[string]JSONSchema{},
+	}
 
-		switch input.Type.Type {
-		case proto.Type_TYPE_ID:
-			prop.Type = "string"
-		case proto.Type_TYPE_STRING:
-			prop.Type = "string"
+	values := JSONSchema{
+		Type:       "object",
+		Properties: map[string]JSONSchema{},
+	}
+
+	isUpdate := op.Type == proto.OperationType_OPERATION_TYPE_UPDATE
+	isList := op.Type == proto.OperationType_OPERATION_TYPE_LIST
+
+	for _, input := range op.Inputs {
+		prop := jsonSchemaForInput(ctx, op, input, schema)
+
+		isWrite := input.Mode == proto.InputMode_INPUT_MODE_WRITE
+		isRead := input.Mode == proto.InputMode_INPUT_MODE_READ
+
+		var obj *JSONSchema
+
+		switch {
+		case isUpdate && isRead, isList:
+			obj = &where
+		case isUpdate && isWrite:
+			obj = &values
+		default:
+			obj = &root
 		}
 
-		s.Properties[input.Name] = prop
+		obj.Properties[input.Name] = prop
 
+		// If the input is not optional then mark it required in the JSON schema
 		if !input.Optional {
-			s.Required = append(s.Required, input.Name)
+			obj.Required = append(obj.Required, input.Name)
 		}
 	}
 
-	return s
+	if isUpdate || isList {
+		// Always add the "where" prop but only make it required if has any properties
+		root.Properties["where"] = where
+		if len(where.Properties) > 0 {
+			root.Required = append(root.Required, "where")
+		}
+	}
+
+	if isUpdate {
+		// Always add the "values" prop but only make it required if has any properties
+		root.Properties["values"] = values
+		if len(values.Properties) > 0 {
+			root.Required = append(root.Required, "values")
+		}
+	}
+
+	return root
+}
+
+func jsonSchemaForInput(ctx context.Context, op *proto.Operation, input *proto.OperationInput, schema *proto.Schema) JSONSchema {
+	if op.Type == proto.OperationType_OPERATION_TYPE_LIST {
+		return jsonSchemaForQueryObject(ctx, op, input, schema)
+	}
+
+	prop := JSONSchema{}
+
+	switch input.Type.Type {
+	case proto.Type_TYPE_ID:
+		prop.Type = "string"
+	case proto.Type_TYPE_STRING:
+		prop.Type = "string"
+	case proto.Type_TYPE_DATE:
+		prop.Type = "string"
+		prop.Format = "date"
+	case proto.Type_TYPE_DATETIME, proto.Type_TYPE_TIMESTAMP:
+		prop.Type = "string"
+		prop.Format = "date-time"
+	case proto.Type_TYPE_BOOL:
+		prop.Type = "boolean"
+	case proto.Type_TYPE_INT:
+		prop.Type = "number"
+	case proto.Type_TYPE_ENUM:
+		prop.Type = "string"
+		enum, _ := lo.Find(schema.Enums, func(e *proto.Enum) bool {
+			return e.Name == input.Type.EnumName.Value
+		})
+		for _, v := range enum.Values {
+			prop.Enum = append(prop.Enum, v.Name)
+		}
+	}
+
+	isImplicit := input.Behaviour == proto.InputBehaviour_INPUT_BEHAVIOUR_IMPLICIT
+	isWrite := input.Mode == proto.InputMode_INPUT_MODE_WRITE
+
+	// An input is allowed to be null if the field it relates to is marked
+	// optional. This is because "optional" has a slightly different meaning
+	// between model fields and action inputs:
+	//     - model field:  "can be null"
+	//     - action input: "can be ommitted"
+	if isImplicit && isWrite {
+		field := proto.FindField(schema.Models, op.ModelName, input.Target[0])
+		if field.Optional {
+			// OpenAPI differs from JSON Schema in that:
+			//   | Note that there is no null type; instead, the nullable
+			//   | attribute is used as a modifier of the base type.
+			// TODO: when we want to support OpenAPI generation we'll need
+			// to support both styles. For now we only support JSON Schema.
+			prop.Type = []any{prop.Type, "null"}
+		}
+	}
+
+	return prop
+}
+
+func jsonSchemaForQueryObject(ctx context.Context, op *proto.Operation, input *proto.OperationInput, schema *proto.Schema) JSONSchema {
+	switch input.Type.Type {
+	case proto.Type_TYPE_ID:
+		t := JSONSchema{
+			Type: "string",
+		}
+		return JSONSchema{
+			Type: "object",
+			Properties: map[string]JSONSchema{
+				"equals": t,
+				"oneOf":  {Type: "array", Items: &t},
+			},
+		}
+	case proto.Type_TYPE_STRING:
+		t := JSONSchema{
+			Type: "string",
+		}
+		return JSONSchema{
+			Type: "object",
+			Properties: map[string]JSONSchema{
+				"equals":     t,
+				"startsWith": t,
+				"endsWith":   t,
+				"contains":   t,
+				"oneOf":      {Type: "array", Items: &t},
+			},
+		}
+	case proto.Type_TYPE_DATE:
+		t := JSONSchema{
+			Type:   "string",
+			Format: "date",
+		}
+		return JSONSchema{
+			Type: "object",
+			Properties: map[string]JSONSchema{
+				"equals":     t,
+				"before":     t,
+				"onOrBefore": t,
+				"after":      t,
+				"onOrAfter":  t,
+			},
+		}
+	case proto.Type_TYPE_DATETIME, proto.Type_TYPE_TIMESTAMP:
+		t := JSONSchema{
+			Type:   "string",
+			Format: "date-time",
+		}
+		return JSONSchema{
+			Type: "object",
+			Properties: map[string]JSONSchema{
+				"before": t,
+				"after":  t,
+			},
+		}
+	case proto.Type_TYPE_BOOL:
+		return JSONSchema{
+			Type: "object",
+			Properties: map[string]JSONSchema{
+				"equals": {Type: "boolean"},
+			},
+		}
+	case proto.Type_TYPE_INT:
+		t := JSONSchema{
+			Type: "number",
+		}
+		return JSONSchema{
+			Type: "object",
+			Properties: map[string]JSONSchema{
+				"equals":              t,
+				"lessThan":            t,
+				"lessThanOrEquals":    t,
+				"greaterThan":         t,
+				"greaterThanOrEquals": t,
+			},
+		}
+	case proto.Type_TYPE_ENUM:
+		t := JSONSchema{
+			Type: "string",
+		}
+		enum, _ := lo.Find(schema.Enums, func(e *proto.Enum) bool {
+			return e.Name == input.Type.EnumName.Value
+		})
+		for _, v := range enum.Values {
+			t.Enum = append(t.Enum, v.Name)
+		}
+		return JSONSchema{
+			Type: "object",
+			Properties: map[string]JSONSchema{
+				"equals": t,
+				"oneOf": {
+					Type:  "array",
+					Items: &t,
+				},
+			},
+		}
+	}
+
+	return JSONSchema{}
 }
--- runtime/apis/rpc/jsonschema_test.go
+++ runtime/apis/rpc/jsonschema_test.go
@@ -3,6 +3,7 @@ package rpc_test
 import (
 	"context"
 	"encoding/json"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,106 +16,605 @@ import (
 func TestValidateRequest(t *testing.T) {
 	type fixture struct {
 		name    string
-		schema  string
 		opName  string
 		request string
 		errors  map[string]string
 	}
 
-	fixtures := []fixture{
+	type fixtureGroup struct {
+		name   string
+		schema string
+		cases  []fixture
+	}
+
+	fixtures := []fixtureGroup{
 		{
-			name: "get action missing input",
-			schema: `
-				model Person {
-					operations {
-						get getPerson(id)
-					}
-				}
-			`,
-			request: `
-				{}
-			`,
-			opName: "getPerson",
-			errors: map[string]string{
-				"(root)": "id is required",
-			},
-		},
-		{
-			name: "get action type id wrong type",
-			schema: `
-				model Person {
-					operations {
-						get getPerson(id)
-					}
-				}
-			`,
-			request: `
-				{
-					"id": 1234
-				}
-			`,
-			opName: "getPerson",
-			errors: map[string]string{
-				"id": "Invalid type. Expected: string, given: integer",
-			},
-		},
-		{
-			name: "get action type text wrong type",
+			name: "get action",
 			schema: `
 				model Person {
 					fields {
 						name Text @unique
 					}
 					operations {
-						get getPerson(name)
+						get getPerson(id)
+						get getBestBeatle() {
+							@where(person.name == "John Lennon")
+						}
 					}
 				}
 			`,
-			request: `
+			cases: []fixture{
 				{
-					"name": 1234
+					name:    "valid - with input",
+					request: `{"id": "1234"}`,
+					opName:  "getPerson",
+				},
+				{
+					name:    "valid - without input",
+					request: `{}`,
+					opName:  "getBestBeatle",
+				},
+
+				// errors
+				{
+					name:    "missing input",
+					request: `{}`,
+					opName:  "getPerson",
+					errors: map[string]string{
+						"(root)": "id is required",
+					},
+				},
+				{
+					name:    "wrong type",
+					request: `{"id": 1234}`,
+					opName:  "getPerson",
+					errors: map[string]string{
+						"id": "Invalid type. Expected: string, given: integer",
+					},
+				},
+				{
+					name:    "null",
+					request: `{"id": null}`,
+					opName:  "getPerson",
+					errors: map[string]string{
+						"id": "Invalid type. Expected: string, given: null",
+					},
+				},
+				{
+					name:    "valid inputs with additional properties",
+					request: `{"id": "1234", "foo": "bar"}`,
+					opName:  "getPerson",
+					errors: map[string]string{
+						"(root)": "Additional property foo is not allowed",
+					},
+				},
+				{
+					name:    "additional properties when no inputs expected",
+					request: `{"id": "1234"}`,
+					opName:  "getBestBeatle",
+					errors: map[string]string{
+						"(root)": "Additional property id is not allowed",
+					},
+				},
+			},
+		},
+		{
+			name: "delete action",
+			schema: `
+				model Person {
+					fields {
+						name Text @unique
+					}
+					operations {
+						delete deletePerson(id)
+						delete deleteBob() {
+							@where(person.name == "Bob")
+						}
+					}
 				}
 			`,
-			opName: "getPerson",
-			errors: map[string]string{
-				"name": "Invalid type. Expected: string, given: integer",
+			cases: []fixture{
+				{
+					name:    "valid - with input",
+					request: `{"id": "1234"}`,
+					opName:  "deletePerson",
+				},
+				{
+					name:    "valid - without input",
+					request: `{}`,
+					opName:  "deleteBob",
+				},
+
+				// errors
+				{
+					name:    "missing input",
+					request: `{}`,
+					opName:  "deletePerson",
+					errors: map[string]string{
+						"(root)": "id is required",
+					},
+				},
+				{
+					name:    "wrong type",
+					request: `{"id": 1234}`,
+					opName:  "deletePerson",
+					errors: map[string]string{
+						"id": "Invalid type. Expected: string, given: integer",
+					},
+				},
+				{
+					name:    "null",
+					request: `{"id": null}`,
+					opName:  "deletePerson",
+					errors: map[string]string{
+						"id": "Invalid type. Expected: string, given: null",
+					},
+				},
+				{
+					name:    "valid inputs with additional properties",
+					request: `{"id": "1234", "foo": "bar"}`,
+					opName:  "deletePerson",
+					errors: map[string]string{
+						"(root)": "Additional property foo is not allowed",
+					},
+				},
+				{
+					name:    "additional properties when no inputs expected",
+					request: `{"id": "1234"}`,
+					opName:  "deleteBob",
+					errors: map[string]string{
+						"(root)": "Additional property id is not allowed",
+					},
+				},
+			},
+		},
+		{
+			name: "create action",
+			schema: `
+				model Person {
+					fields {
+						name Text
+						birthday Date?
+					}
+					operations {
+						create createPerson() with (name)
+						create createPersonWithDOB() with (name, birthday)
+						create createPersonWithOptionalDOB() with (name, birthday?)
+					}
+				}
+			`,
+			cases: []fixture{
+				{
+					name:    "valid - basic",
+					request: `{"name": "Jon"}`,
+					opName:  "createPerson",
+				},
+				{
+					name:    "valid - input for optional field provided",
+					request: `{"name": "Jon", "birthday": "1986-03-18"}`,
+					opName:  "createPersonWithDOB",
+				},
+				{
+					name:    "valid - input for optional field provided as null",
+					request: `{"name": "Jon", "birthday": null}`,
+					opName:  "createPersonWithDOB",
+				},
+				{
+					name:    "valid - ommitting optional input",
+					request: `{"name": "Jon"}`,
+					opName:  "createPersonWithOptionalDOB",
+				},
+				{
+					name:    "valid - providing optional input for optional field as null",
+					request: `{"name": "Jon", "birthday": null}`,
+					opName:  "createPersonWithOptionalDOB",
+				},
+				{
+					name:    "valid - providing optional input for optional field",
+					request: `{"name": "Jon", "birthday": "1986-03-18"}`,
+					opName:  "createPersonWithOptionalDOB",
+				},
+
+				// errors
+				{
+					name:    "missing input",
+					request: `{}`,
+					opName:  "createPerson",
+					errors: map[string]string{
+						"(root)": "name is required",
+					},
+				},
+				{
+					name:    "missing required input for optional field",
+					request: `{"name": "Jon"}`,
+					opName:  "createPersonWithDOB",
+					errors: map[string]string{
+						"(root)": "birthday is required",
+					},
+				},
+				{
+					name:    "null",
+					request: `{"name": null}`,
+					opName:  "createPerson",
+					errors: map[string]string{
+						"name": "Invalid type. Expected: string, given: null",
+					},
+				},
+				{
+					name:    "wrong type",
+					request: `{"name": 1234}`,
+					opName:  "createPerson",
+					errors: map[string]string{
+						"name": "Invalid type. Expected: string, given: integer",
+					},
+				},
+				{
+					name:    "wrong format for date",
+					request: `{"name": "Jon", "birthday": "18th March 1986"}`,
+					opName:  "createPersonWithDOB",
+					errors: map[string]string{
+						"birthday": "Does not match format 'date'",
+					},
+				},
+				{
+					name:    "additional properties",
+					request: `{"name": "Bob", "foo": "bar"}`,
+					opName:  "createPerson",
+					errors: map[string]string{
+						"(root)": "Additional property foo is not allowed",
+					},
+				},
+			},
+		},
+		{
+			name: "update action",
+			schema: `
+				model Person {
+					fields {
+						identity Identity @unique
+						name Text
+						nickName Text?
+					}
+					operations {
+						update updateName(id) with (name)
+						update updateNameAndNickname(id) with (name, nickName)
+						update updateNameOrNickname(id) with (name?, nickName?)
+						update updateMyPerson() {
+							@where(person.identity == ctx.identity)
+							@set(person.name = "Hello")
+						}
+						update updateMyPersonWithName() with (name) {
+							@where(person.identity == ctx.identity)
+						}
+					}
+				}
+			`,
+			cases: []fixture{
+				{
+					name:    "valid - one input",
+					request: `{"where": {"id": "1234"}, "values": {"name": "Jon"}}`,
+					opName:  "updateName",
+				},
+				{
+					name:    "valid - two inputs",
+					request: `{"where": {"id": "1234"}, "values": {"name": "Jon", "nickName": "Johnny"}}`,
+					opName:  "updateNameAndNickname",
+				},
+				{
+					name:    "valid - two inputs - null for optional field",
+					request: `{"where": {"id": "1234"}, "values": {"name": "Jon", "nickName": null}}`,
+					opName:  "updateNameAndNickname",
+				},
+				{
+					name:    "valid - two inputs - both optional - both provided",
+					request: `{"where": {"id": "1234"}, "values": {"name": "Jon", "nickName": "Johnny"}}`,
+					opName:  "updateNameOrNickname",
+				},
+				{
+					name:    "valid - two inputs - both optional - one provided",
+					request: `{"where": {"id": "1234"}, "values": {"nickName": "Johnny"}}`,
+					opName:  "updateNameOrNickname",
+				},
+				{
+					name:    "valid - two inputs - both optional - neither provided",
+					request: `{"where": {"id": "1234"}, "values": {}}`,
+					opName:  "updateNameOrNickname",
+				},
+				{
+					name:    "valid - no inputs - empty request is ok",
+					request: `{}`,
+					opName:  "updateMyPerson",
+				},
+				{
+					name:    "valid - no inputs - empty where and values is ok",
+					request: `{"where": {}, "values": {}}`,
+					opName:  "updateMyPerson",
+				},
+				{
+					name:    "valid - values but no where",
+					request: `{"values": {"name": "Jon"}}`,
+					opName:  "updateMyPersonWithName",
+				},
+
+				// errors
+				{
+					name:    "missing required value",
+					request: `{"where": {"id": "1234"}, "values": {}}`,
+					opName:  "updateName",
+					errors: map[string]string{
+						"values": "name is required",
+					},
+				},
+				{
+					name:    "missing required where",
+					request: `{"where": {}, "values": {"name": "Jon"}}`,
+					opName:  "updateName",
+					errors: map[string]string{
+						"where": "id is required",
+					},
+				},
+				{
+					name:    "incorrect type for value",
+					request: `{"where": {"id": "1234"}, "values": {"name": true}}`,
+					opName:  "updateName",
+					errors: map[string]string{
+						"values.name": "Invalid type. Expected: string, given: boolean",
+					},
+				},
+				{
+					name:    "incorrect type for where",
+					request: `{"where": {"id": 1234}, "values": {"name": "Jon"}}`,
+					opName:  "updateName",
+					errors: map[string]string{
+						"where.id": "Invalid type. Expected: string, given: integer",
+					},
+				},
+			},
+		},
+		{
+			name: "list action",
+			schema: `
+				enum Genre {
+					Romance
+					Horror
+				}
+				model Author {
+					fields {
+						name Text
+					}
+				}
+				model Book {
+					fields {
+						author Author
+						title Text
+						genre Genre
+						price Number
+						available Boolean
+						releaseDate Date
+					}
+					operations {
+						list listBooks(id?, title?, genre?, price?, available?, createdAt?, releaseDate?)
+					}
+				}
+			`,
+			cases: []fixture{
+				{
+					name:    "valid - no inputs",
+					opName:  "listBooks",
+					request: `{"where": {}}`,
+				},
+				{
+					name:    "valid - text equals",
+					opName:  "listBooks",
+					request: `{"where": {"title": {"equals": "Great Gatsby"}}}`,
+				},
+				{
+					name:    "valid - text starts with",
+					opName:  "listBooks",
+					request: `{"where": {"title": {"startsWith": "Great Gatsby"}}}`,
+				},
+				{
+					name:    "valid - text ends with",
+					opName:  "listBooks",
+					request: `{"where": {"title": {"startsWith": "Great Gatsby"}}}`,
+				},
+				{
+					name:    "valid - text contains",
+					opName:  "listBooks",
+					request: `{"where": {"title": {"startsWith": "Great Gatsby"}}}`,
+				},
+				{
+					name:    "valid - text one of",
+					opName:  "listBooks",
+					request: `{"where": {"title": {"oneOf": ["Great Gatsby", "Ulysses"]}}}`,
+				},
+				{
+					name:    "valid - text multi",
+					opName:  "listBooks",
+					request: `{"where": {"title": {"startsWith": "Great", "endsWith": "Gatsby"}}}`,
+				},
+				{
+					name:    "valid - enum equals",
+					opName:  "listBooks",
+					request: `{"where": {"genre": {"equals": "Romance"}}}`,
+				},
+				{
+					name:    "valid - enum one of",
+					opName:  "listBooks",
+					request: `{"where": {"genre": {"oneOf": ["Romance", "Horror"]}}}`,
+				},
+				{
+					name:    "valid - number equals",
+					opName:  "listBooks",
+					request: `{"where": {"price": {"equals": 10}}}`,
+				},
+				{
+					name:    "valid - number less than",
+					opName:  "listBooks",
+					request: `{"where": {"price": {"lessThan": 10}}}`,
+				},
+				{
+					name:    "valid - number greater than",
+					opName:  "listBooks",
+					request: `{"where": {"price": {"greaterThan": 10}}}`,
+				},
+				{
+					name:    "valid - number less than or equals",
+					opName:  "listBooks",
+					request: `{"where": {"price": {"lessThanOrEquals": 10}}}`,
+				},
+				{
+					name:    "valid - number greater than or equals",
+					opName:  "listBooks",
+					request: `{"where": {"price": {"greaterThanOrEquals": 10}}}`,
+				},
+				{
+					name:    "valid - boolean equals",
+					opName:  "listBooks",
+					request: `{"where": {"available": {"equals": true}}}`,
+				},
+				{
+					name:    "valid - timestamp before",
+					opName:  "listBooks",
+					request: `{"where": {"createdAt": {"before": "2022-12-02T12:28:29.609Z"}}}`,
+				},
+				{
+					name:    "valid - timestamp after",
+					opName:  "listBooks",
+					request: `{"where": {"createdAt": {"after": "2022-12-02T12:28:29.609Z"}}}`,
+				},
+				{
+					name:    "valid - date equals",
+					opName:  "listBooks",
+					request: `{"where": {"releaseDate": {"equals": "2022-12-02"}}}`,
+				},
+				{
+					name:    "valid - date before",
+					opName:  "listBooks",
+					request: `{"where": {"releaseDate": {"before": "2022-12-02"}}}`,
+				},
+				{
+					name:    "valid - date on or before",
+					opName:  "listBooks",
+					request: `{"where": {"releaseDate": {"onOrBefore": "2022-12-02"}}}`,
+				},
+				{
+					name:    "valid - date after",
+					opName:  "listBooks",
+					request: `{"where": {"releaseDate": {"after": "2022-12-02"}}}`,
+				},
+				{
+					name:    "valid - date on or after",
+					opName:  "listBooks",
+					request: `{"where": {"releaseDate": {"onOrAfter": "2022-12-02"}}}`,
+				},
+				{
+					name:    "valid - id equals",
+					opName:  "listBooks",
+					request: `{"where": {"id": {"equals": "123456789"}}}`,
+				},
+				{
+					name:    "valid - id one of",
+					opName:  "listBooks",
+					request: `{"where": {"id": {"oneOf": ["123456789"]}}}`,
+				},
+
+				// errors
+				{
+					name:    "text unknown filter",
+					opName:  "listBooks",
+					request: `{"where": {"title": {"isSimilarTo": "Sci-fi"}}}`,
+					errors: map[string]string{
+						"where.title": `Additional property isSimilarTo is not allowed`,
+					},
+				},
+				{
+					name:    "enum equals not valid enum",
+					opName:  "listBooks",
+					request: `{"where": {"genre": {"equals": "Sci-fi"}}}`,
+					errors: map[string]string{
+						"where.genre.equals": `where.genre.equals must be one of the following: "Romance", "Horror"`,
+					},
+				},
+				{
+					name:    "enum one of not valid enum",
+					opName:  "listBooks",
+					request: `{"where": {"genre": {"oneOf": ["Sci-fi"]}}}`,
+					errors: map[string]string{
+						"where.genre.oneOf.0": `where.genre.oneOf.0 must be one of the following: "Romance", "Horror"`,
+					},
+				},
+				{
+					name:    "timestamp invalid format",
+					opName:  "listBooks",
+					request: `{"where": {"createdAt": {"after": "not-a-date-time"}}}`,
+					errors: map[string]string{
+						"where.createdAt.after": `Does not match format 'date-time'`,
+					},
+				},
+				{
+					name:    "date invalid format",
+					opName:  "listBooks",
+					request: `{"where": {"releaseDate": {"after": "not-a-date-time"}}}`,
+					errors: map[string]string{
+						"where.releaseDate.after": `Does not match format 'date'`,
+					},
+				},
 			},
 		},
 	}
 
-	for _, f := range fixtures {
-		t.Run(f.name, func(t *testing.T) {
+	for _, group := range fixtures {
+		for _, f := range group.cases {
+			group := group
+			f := f
+			t.Run(group.name+"/"+f.name, func(t *testing.T) {
 
-			builder := schema.Builder{}
-			schema, err := builder.MakeFromString(f.schema)
-			require.NoError(t, err)
+				builder := schema.Builder{}
+				schema, err := builder.MakeFromString(group.schema)
+				require.NoError(t, err)
 
-			var req map[string]any
-			err = json.Unmarshal([]byte(f.request), &req)
-			require.NoError(t, err)
+				var req map[string]any
+				err = json.Unmarshal([]byte(f.request), &req)
+				require.NoError(t, err)
 
-			op := proto.FindOperation(schema, f.opName)
+				op := proto.FindOperation(schema, f.opName)
 
-			result, err := rpc.ValidateRequest(context.Background(), schema, op, req)
-			require.NoError(t, err)
-			require.NotNil(t, result)
+				result, err := rpc.ValidateRequest(context.Background(), schema, op, req)
+				require.NoError(t, err)
+				require.NotNil(t, result)
 
-			for _, e := range result.Errors() {
-				expected, ok := f.errors[e.Field()]
-				if !ok {
-					assert.Fail(t, "unexpected error", "%s - %s", e.Field(), e.Description())
-					continue
+				if len(f.errors) == 0 {
+					assert.True(t, result.Valid(), "expected request to be valid")
 				}
 
-				assert.Equal(t, expected, e.Description(), "error for field %s did not match expected", e.Field())
-				delete(f.errors, e.Field())
-			}
+				for _, e := range result.Errors() {
+					// this will be the full path to where the error is located
+					// for example "(root).someProp.someOtherProp"
+					jsonPath := e.Context().String()
 
-			// f.errors should now be empty, if not mark test as failed
-			for field, description := range f.errors {
-				assert.Fail(t, "expected error was not returned", "%s - %s", field, description)
-			}
-		})
+					// When we're not at the root, we don't really need that
+					// prefix, so trim it off
+					if jsonPath != "(root)" {
+						jsonPath = strings.TrimPrefix(jsonPath, "(root).")
+					}
+
+					expected, ok := f.errors[jsonPath]
+					if !ok {
+						assert.Fail(t, "unexpected error", "%s - %s", jsonPath, e.Description())
+						continue
+					}
+
+					assert.Equal(t, expected, e.Description(), "error for path %s did not match expected", jsonPath)
+					delete(f.errors, jsonPath)
+				}
+
+				// f.errors should now be empty, if not mark test as failed
+				for path, description := range f.errors {
+					assert.Fail(t, "expected error was not returned", "%s - %s", path, description)
+				}
+			})
+		}
 	}
 
 }
