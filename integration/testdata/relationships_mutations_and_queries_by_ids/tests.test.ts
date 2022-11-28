e0256a4c34a8fa43eb0625f145da131b8a99d8e1
Dave New <sanodn@gmail.com>
2022-11-28 10:43:07 +0200

    chore: prettier fixes

--- integration/testdata/relationships_mutations_and_queries_by_ids/tests.test.ts
+++ integration/testdata/relationships_mutations_and_queries_by_ids/tests.test.ts
@@ -1,15 +1,11 @@
-import {
-  test,
-  expect,
-  actions,
-  Post,
-  Author,
-} from "@teamkeel/testing";
-
+import { test, expect, actions, Post, Author } from "@teamkeel/testing";
 
 test("create with parent id as implicit input - get by id - parent id set correctly", async () => {
   const { object: author } = await actions.createAuthor({ name: "Keelson" });
-  const { object: post } = await actions.createPost({ title: "Keelson Post", theAuthorId: author.id });
+  const { object: post } = await actions.createPost({
+    title: "Keelson Post",
+    theAuthorId: author.id,
+  });
 
   expect(post.theAuthorId).toEqual(author.id);
 
@@ -21,7 +17,10 @@ test("create with parent id as implicit input - get by id - parent id set correc
 
 test("create with parent id as implicit input foreign key - get by id - parent id set correctly", async () => {
   const { object: author } = await actions.createAuthor({ name: "Keelson" });
-  const { object: post } = await actions.createPostForeignKey({ title: "Keelson Post", theAuthorId: author.id });
+  const { object: post } = await actions.createPostForeignKey({
+    title: "Keelson Post",
+    theAuthorId: author.id,
+  });
 
   expect(post.theAuthorId).toEqual(author.id);
 
@@ -33,7 +32,10 @@ test("create with parent id as implicit input foreign key - get by id - parent i
 
 test("create with parent id with set attribute - get by id - parent id set correctly", async () => {
   const { object: author } = await actions.createAuthor({ name: "Keelson" });
-  const { object: post } = await actions.createPostWithSet({ title: "Keelson Post", explicitAuthorId: author.id });
+  const { object: post } = await actions.createPostWithSet({
+    title: "Keelson Post",
+    explicitAuthorId: author.id,
+  });
 
   expect(post.theAuthorId).toEqual(author.id);
 
@@ -43,10 +45,12 @@ test("create with parent id with set attribute - get by id - parent id set corre
   expect(getPost.title).toEqual("Keelson Post");
 });
 
-
 test("update parent id as implicit input - get by id - parent id updated correctly", async () => {
   const { object: author1 } = await actions.createAuthor({ name: "Keelson" });
-  const { object: post } = await actions.createPost({ title: "Keelson Post", theAuthorId: author1.id });
+  const { object: post } = await actions.createPost({
+    title: "Keelson Post",
+    theAuthorId: author1.id,
+  });
   const { object: author2 } = await actions.createAuthor({ name: "Weaveton" });
 
   const { object: getPost } = await actions.getPost({ id: post.id });
@@ -54,9 +58,9 @@ test("update parent id as implicit input - get by id - parent id updated correct
   expect(getPost.theAuthorId).toEqual(author1.id);
   expect(getPost.title).toEqual("Keelson Post");
 
-  const { object: updatePost } = await actions.updatePost({ 
-    where: { id: post.id }, 
-    values: { title: "Updated", theAuthorId: author2.id } 
+  const { object: updatePost } = await actions.updatePost({
+    where: { id: post.id },
+    values: { title: "Updated", theAuthorId: author2.id },
   });
 
   const { object: getUpdatedPost } = await actions.getPost({ id: post.id });
@@ -67,7 +71,10 @@ test("update parent id as implicit input - get by id - parent id updated correct
 
 test("update parent id as implicit input foreign key - get by id - parent id updated correctly", async () => {
   const { object: author1 } = await actions.createAuthor({ name: "Keelson" });
-  const { object: post } = await actions.createPost({ title: "Keelson Post", theAuthorId: author1.id });
+  const { object: post } = await actions.createPost({
+    title: "Keelson Post",
+    theAuthorId: author1.id,
+  });
   const { object: author2 } = await actions.createAuthor({ name: "Weaveton" });
 
   const { object: getPost } = await actions.getPost({ id: post.id });
@@ -75,9 +82,9 @@ test("update parent id as implicit input foreign key - get by id - parent id upd
   expect(getPost.theAuthorId).toEqual(author1.id);
   expect(getPost.title).toEqual("Keelson Post");
 
-  const { object: updatePost } = await actions.updatePostForeignKey({ 
-    where: { id: post.id }, 
-    values: { title: "Updated", theAuthorId: author2.id } 
+  const { object: updatePost } = await actions.updatePostForeignKey({
+    where: { id: post.id },
+    values: { title: "Updated", theAuthorId: author2.id },
   });
 
   const { object: getUpdatedPost } = await actions.getPost({ id: post.id });
@@ -88,7 +95,10 @@ test("update parent id as implicit input foreign key - get by id - parent id upd
 
 test("update parent id as implicit input with set attribute - get by id - parent id updated correctly", async () => {
   const { object: author1 } = await actions.createAuthor({ name: "Keelson" });
-  const { object: post } = await actions.createPost({ title: "Keelson Post", theAuthorId: author1.id });
+  const { object: post } = await actions.createPost({
+    title: "Keelson Post",
+    theAuthorId: author1.id,
+  });
   const { object: author2 } = await actions.createAuthor({ name: "Weaveton" });
 
   const { object: getPost } = await actions.getPost({ id: post.id });
@@ -96,9 +106,9 @@ test("update parent id as implicit input with set attribute - get by id - parent
   expect(getPost.theAuthorId).toEqual(author1.id);
   expect(getPost.title).toEqual("Keelson Post");
 
-  const { object: updatePost } = await actions.updatePostWithSet({ 
-    where: { id: post.id }, 
-    values: { title: "Updated", explicitAuthorId: author2.id } 
+  const { object: updatePost } = await actions.updatePostWithSet({
+    where: { id: post.id },
+    values: { title: "Updated", explicitAuthorId: author2.id },
   });
 
   const { object: getUpdatedPost } = await actions.getPost({ id: post.id });
@@ -109,11 +119,20 @@ test("update parent id as implicit input with set attribute - get by id - parent
 
 test("get filter by parent id - get by id and parent id - filtered correctly", async () => {
   const { object: author1 } = await actions.createAuthor({ name: "Keelson" });
-  const { object: post1 } = await actions.createPost({ title: "Keelson Post", theAuthorId: author1.id });
+  const { object: post1 } = await actions.createPost({
+    title: "Keelson Post",
+    theAuthorId: author1.id,
+  });
   const { object: author2 } = await actions.createAuthor({ name: "Weaveton" });
-  const { object: post2 } = await actions.createPost({ title: "Weaveton Post", theAuthorId: author2.id });
+  const { object: post2 } = await actions.createPost({
+    title: "Weaveton Post",
+    theAuthorId: author2.id,
+  });
 
-  const { object: getPost1 } = await actions.getPostByAuthor({ id: post1.id, theAuthorId: author1.id });
+  const { object: getPost1 } = await actions.getPostByAuthor({
+    id: post1.id,
+    theAuthorId: author1.id,
+  });
   expect(getPost1.id).toEqual(post1.id);
   expect(getPost1.theAuthorId).toEqual(author1.id);
 
@@ -126,11 +145,19 @@ test("get filter by parent id - get by id and parent id - filtered correctly", a
 
 test("list filter by parent id - list and parent id - filtered correctly", async () => {
   const { object: author1 } = await actions.createAuthor({ name: "Keelson" });
-  const { object: post1 } = await actions.createPost({ title: "Keelson Post", theAuthorId: author1.id });
+  const { object: post1 } = await actions.createPost({
+    title: "Keelson Post",
+    theAuthorId: author1.id,
+  });
   const { object: author2 } = await actions.createAuthor({ name: "Weaveton" });
-  const { object: post2 } = await actions.createPost({ title: "Weaveton Post", theAuthorId: author2.id });
+  const { object: post2 } = await actions.createPost({
+    title: "Weaveton Post",
+    theAuthorId: author2.id,
+  });
 
-  const { collection: listPost } = await actions.listPost({ where: { theAuthorId: { equals: author1.id } } });
+  const { collection: listPost } = await actions.listPost({
+    where: { theAuthorId: { equals: author1.id } },
+  });
   expect(listPost.length).toEqual(1);
   expect(listPost[0].id).toEqual(post1.id);
   expect(listPost[0].theAuthorId).toEqual(author1.id);
@@ -139,11 +166,20 @@ test("list filter by parent id - list and parent id - filtered correctly", async
 
 test("get filter by child id - get by id and parent id - filtered correctly", async () => {
   const { object: author1 } = await actions.createAuthor({ name: "Keelson" });
-  const { object: post1 } = await actions.createPost({ title: "Keelson Post", theAuthorId: author1.id });
+  const { object: post1 } = await actions.createPost({
+    title: "Keelson Post",
+    theAuthorId: author1.id,
+  });
   const { object: author2 } = await actions.createAuthor({ name: "Weaveton" });
-  const { object: post2 } = await actions.createPost({ title: "Weaveton Post", theAuthorId: author2.id });
+  const { object: post2 } = await actions.createPost({
+    title: "Weaveton Post",
+    theAuthorId: author2.id,
+  });
 
-  const { object: getAuthor1 } = await actions.getAuthorByPost({ id: author1.id, thePostsId: post1.id });
+  const { object: getAuthor1 } = await actions.getAuthorByPost({
+    id: author1.id,
+    thePostsId: post1.id,
+  });
   expect(getAuthor1.id).toEqual(author1.id);
   expect(getAuthor1.name).toEqual(author1.name);
 
@@ -156,11 +192,19 @@ test("get filter by child id - get by id and parent id - filtered correctly", as
 
 test("list filter by parent id - list and parent id - filtered correctly", async () => {
   const { object: author1 } = await actions.createAuthor({ name: "Keelson" });
-  const { object: post1 } = await actions.createPost({ title: "Keelson Post", theAuthorId: author1.id });
+  const { object: post1 } = await actions.createPost({
+    title: "Keelson Post",
+    theAuthorId: author1.id,
+  });
   const { object: author2 } = await actions.createAuthor({ name: "Weaveton" });
-  const { object: post2 } = await actions.createPost({ title: "Weaveton Post", theAuthorId: author2.id });
+  const { object: post2 } = await actions.createPost({
+    title: "Weaveton Post",
+    theAuthorId: author2.id,
+  });
 
-  const { collection: listAuthor } = await actions.listAuthors({ where: { thePostsId: { equals: post1.id } } });
+  const { collection: listAuthor } = await actions.listAuthors({
+    where: { thePostsId: { equals: post1.id } },
+  });
   expect(listAuthor.length).toEqual(1);
   expect(listAuthor[0].id).toEqual(author1.id);
   expect(listAuthor[0].name).toEqual(author1.name);
