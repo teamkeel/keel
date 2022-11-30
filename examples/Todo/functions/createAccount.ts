973dd04391246fc7d1d296e420bf4bd88842a2c6
Adam Bull <adam@keel.xyz>
2022-11-30 13:09:10 +0000

    chore: remove examples (#632)

--- examples/Todo/functions/createAccount.ts
+++
@@ -1,7 +0,0 @@
-import { CreateAccountInput, API } from '@teamkeel/sdk'
-
-export default async (inputs: CreateAccountInput, api: API) => {
-  // Build your universe
-
-  api.models.Account.create({ name: 'my account' })
-}
