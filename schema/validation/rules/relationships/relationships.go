ef3a04f9eab9f26ebe88262dda23ece4f2310115
peterhoward42 <peterhoward42@gmail.com>
2022-11-30 14:08:08 +0000

    fix: validation rule incorrect logic (#633)
    Co-authored-by: peterhoward42 <peter@keel.xyz>

--- schema/validation/rules/relationships/relationships.go
+++ schema/validation/rules/relationships/relationships.go
@@ -115,12 +115,21 @@ func InvalidImplicitBelongsToWithHasManyRule(asts []*parser.AST) (errs errorhand
 // This is because we have no other way (at present) to infer which field of ModelB to use
 // in the SQL generated associated with ModelA's field.
 func MoreThanOneReverseMany(asts []*parser.AST) (errs errorhandling.ValidationErrors) {
+
+	type hasManyField struct {
+		theField  *parser.FieldNode
+		belongsTo *parser.ModelNode
+	}
+
 	// First capture all the relation fields defined by the ASTs, which are of type HasMany.
-	hasManyFields := []*parser.FieldNode{}
+	hasManyFields := []*hasManyField{}
 	for _, model := range query.Models(asts) {
 		for _, f := range query.ModelFields(model) {
 			if query.IsHasManyModelField(asts, f) {
-				hasManyFields = append(hasManyFields, f)
+				hasManyFields = append(hasManyFields, &hasManyField{
+					theField:  f,
+					belongsTo: model,
+				})
 			}
 		}
 	}
@@ -128,12 +137,24 @@ func MoreThanOneReverseMany(asts []*parser.AST) (errs errorhandling.ValidationEr
 	// Now we iterate over all the captured HasMany relation fields in order to investigate
 	// the model at the HasOne end of the relationship.
 	for _, hasManyF := range hasManyFields {
-		singleEndModel := query.Model(asts, hasManyF.Type)
+
+		// todo: pch check this for nil
+		singleEndModel := query.Model(asts, hasManyF.theField.Type)
 
 		// Given access to the model at the HasOne end, how many fields does it have that
 		// refer back to the model at the hasMany end?
 		reverseFields := query.ModelFields(singleEndModel, func(f *parser.FieldNode) bool {
-			return query.IsHasOneModelField(asts, f) && singleEndModel.Name.Value == hasManyF.Type
+
+			// It can't be a reverse relation field if it's not a hasOne relation field.
+			if !query.IsHasOneModelField(asts, f) {
+				return false
+			}
+			// It isn't a REVERSE relation field if despite it being a hasOne relation field,
+			// it refers to a different model to that of the model to which the hasManyField belongs to.
+			if f.Type != hasManyF.belongsTo.Name.Value {
+				return false
+			}
+			return true
 		})
 
 		// It is an error, if there are more than one such reverse fields.
