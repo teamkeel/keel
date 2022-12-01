117e3daac9a5083510e9f8f848181207e2d415d3
Hugo Sousa <gi.ciberon@gmail.com>
2022-12-01 13:15:19 +0100

    fix: test not to throw

--- packages/functions-runtime/src/db/resolver.test.ts
+++ packages/functions-runtime/src/db/resolver.test.ts
@@ -5,13 +5,15 @@ test("queryResolverFromEnv throws", () => {
 });
 
 test("queryResolverFromEnv dataapi", () => {
-  queryResolverFromEnv({
-    DB_CONN_TYPE: "dataapi",
-    DB_REGION: "eu-west-2",
-    DB_RESOURCE_ARN:
-      "arn:aws:rds:eu-west-2:124567901011:cluster:dev-keel-sharedstagingdb",
-    DB_SECRET_ARN:
-      "arn:aws:rds:eu-west-2:124567901011:cluster:dev-keel-sharedstagingdb",
-    DB_NAME: "env_2H5IwJ1PXKGtwBvkdProxySUWlt",
-  });
+  expect(() =>
+    queryResolverFromEnv({
+      DB_CONN_TYPE: "dataapi",
+      DB_REGION: "eu-west-2",
+      DB_RESOURCE_ARN:
+        "arn:aws:rds:eu-west-2:124567901011:cluster:dev-keel-sharedstagingdb",
+      DB_SECRET_ARN:
+        "arn:aws:rds:eu-west-2:124567901011:cluster:dev-keel-sharedstagingdb",
+      DB_NAME: "env_2H5IwJ1PXKGtwBvkdProxySUWlt",
+    })
+  ).not.toThrow();
 });
