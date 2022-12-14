2560d246c58ae2708a88bf065ba00078db777e9d
Hugo Sousa <gi.ciberon@gmail.com>
2022-12-14 14:23:42 +0100

    feat: add @default validation for expression required

--- schema/validation/rules/expression/expression.go
+++ schema/validation/rules/expression/expression.go
@@ -20,6 +20,30 @@ func ValidateExpression(asts []*parser.AST, expression *parser.Expression, rules
 	return errors
 }
 
+// Validates that the field type has a zero value (no expression necessary).
+// Zero values are the following:
+// * Text -> ""
+// * Number => 0
+// * Boolean -> false
+// * ID -> a ksuid
+// * Timestamp -> now
+func DefaultCanUseZeroValueRule(asts []*parser.AST, attr *parser.AttributeNode, context expressions.ExpressionContext) (errors []*errorhandling.ValidationError) {
+	typesWithZeroValue := []string{"Text", "Number", "Boolean", "ID", "Timestamp"}
+
+	if !lo.Contains(typesWithZeroValue, context.Field.Type) {
+		errors = append(errors,
+			errorhandling.NewValidationError(
+				errorhandling.ErrorDefaultExpressionNeeded,
+				errorhandling.TemplateLiterals{},
+				attr,
+			),
+		)
+		return errors
+	}
+
+	return errors
+}
+
 // Validates that the expression has a single value and it matches the expected type
 func ValueTypechecksRule(asts []*parser.AST, expression *parser.Expression, context expressions.ExpressionContext) (errors []error) {
 	conditions := expression.Conditions()
