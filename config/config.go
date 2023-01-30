68362e7e3bbf8abc072fc5748391038fa03e23da
David Blooman <david.blooman@gmail.com>
2023-01-30 17:29:04 +0000

    feat: validation rules for config require and duplicates

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"gopkg.in/yaml.v3"
 )
@@ -42,5 +43,107 @@ func Load(dir string) (*ProjectConfig, error) {
 		return nil, fmt.Errorf("could not unmarshal config file: %w", err)
 	}
 
+	err = Validate(&config)
+	if err != nil {
+		return &config, fmt.Errorf("could not validate config file: %w", err)
+	}
+
 	return &config, nil
+}
+
+func Validate(config *ProjectConfig) error {
+	duplicates, results := checkForDuplicates(config)
+	if duplicates {
+		for k, v := range results {
+			return fmt.Errorf("duplicate environment variables found in %s: %v", k, strings.Join(v[:], ","))
+		}
+	}
+
+	missingKeys, keys := requiredValuesKeys(config)
+	if missingKeys {
+		for k, v := range keys {
+			return fmt.Errorf("missing required environment variables in %s: %v", k, strings.Join(v[:], ","))
+		}
+	}
+
+	return nil
+}
+func checkForDuplicates(config *ProjectConfig) (bool, map[string][]string) {
+	stagingDuplicates, staging := getDuplicates(config.Environment.Staging)
+	productionDuplicates, production := getDuplicates(config.Environment.Production)
+
+	results := make(map[string][]string, 2)
+
+	if len(staging) > 0 {
+		results["staging"] = staging
+	}
+	if len(production) > 0 {
+		results["production"] = production
+	}
+
+	if stagingDuplicates || productionDuplicates {
+		return true, results
+	}
+
+	return false, results
+}
+
+func getDuplicates(environment []Input) (bool, []string) {
+	allKeys := make(map[string]bool)
+
+	dupes := []string{}
+	for _, item := range environment {
+		if _, value := allKeys[item.Name]; !value {
+			allKeys[item.Name] = true
+		} else {
+			dupes = append(dupes, item.Name)
+		}
+	}
+
+	return len(dupes) > 0, dupes
+}
+
+func requiredValuesKeys(config *ProjectConfig) (bool, map[string][]string) {
+	var staging []string
+	var production []string
+
+	for _, v := range config.Environment.Default {
+		if v.Required == nil {
+			continue
+		}
+
+		for _, required := range v.Required {
+			if required == "staging" {
+				if !contains(config.Environment.Staging, v.Name) {
+					staging = append(staging, v.Name)
+				}
+			}
+			if required == "production" {
+				if !contains(config.Environment.Production, v.Name) {
+					production = append(production, v.Name)
+				}
+			}
+		}
+	}
+
+	results := make(map[string][]string, 2)
+
+	if len(staging) > 0 {
+		results["staging"] = staging
+	}
+
+	if len(production) > 0 {
+		results["production"] = production
+	}
+
+	return len(results) > 0, results
+}
+
+func contains(s []Input, e string) bool {
+	for _, a := range s {
+		if a.Name == e {
+			return true
+		}
+	}
+	return false
 }
--- config/config_test.go
+++ config/config_test.go
@@ -21,3 +21,41 @@ func TestFailConfigValue(t *testing.T) {
 
 	assert.Equal(t, "could not unmarshal config file: yaml: unmarshal errors:\n  line 5: cannot unmarshal !!seq into string", err.Error())
 }
+
+func TestDuplicates(t *testing.T) {
+	config, err := Load("fixtures/test_basic_config_duplicates.yaml")
+	assert.Error(t, err)
+	assert.Equal(t, config.Environment.Default[0].Name, "TEST")
+	assert.Equal(t, "could not validate config file: duplicate environment variables found in staging: TEST", err.Error())
+}
+
+func TestRequiredFail(t *testing.T) {
+	config, err := Load("fixtures/test_basic_config_required_fail.yaml")
+	assert.Error(t, err)
+	assert.Equal(t, config.Environment.Staging[0].Name, "TEST")
+	assert.Equal(t, "could not validate config file: missing required environment variables in production: TEST", err.Error())
+}
+
+func TestRequired(t *testing.T) {
+	config, err := Load("fixtures/test_basic_config_required.yaml")
+	assert.NoError(t, err)
+	assert.Equal(t, config.Environment.Staging[0].Name, "TEST")
+	assert.Equal(t, config.Environment.Production[0].Name, "TEST")
+}
+
+func TestRequiredValuesKeys(t *testing.T) {
+	config, err := Load("fixtures/test_basic_config_required.yaml")
+	assert.NoError(t, err)
+
+	notOk, keys := requiredValuesKeys(config)
+	assert.False(t, notOk)
+	var nil []string
+	assert.Equal(t, nil, keys["staging"])
+
+	config, err = Load("fixtures/test_basic_config_required_fail.yaml")
+	assert.Error(t, err)
+
+	notOk, keys = requiredValuesKeys(config)
+	assert.True(t, notOk)
+	assert.Equal(t, []string{"TEST"}, keys["production"])
+}
