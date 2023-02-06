c859fddd31dd1516abc69754f7f708dcd35b783a
peterhoward42 <peter@keel.xyz>
2023-02-06 09:22:54 +0000

    chore: remove scratch directory

--- integration/testdata/scratch/functions/createPerson.ts
+++
@@ -1,9 +0,0 @@
-import { CreatePerson, fetch} from "@teamkeel/sdk";
-
-export default CreatePerson(async (inputs, api) => {
-
-  const foo = await fetch("garbage url")
-
-  return api.models.person.create(inputs);
-});
-
--- integration/testdata/scratch/functions/getPerson.ts
+++
@@ -1,5 +0,0 @@
-import { GetPerson } from "@teamkeel/sdk";
-
-export default GetPerson((inputs, api) => {
-  return api.models.person.findOne(inputs);
-});
