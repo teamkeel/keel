134423169689f24e41f7e9c3f043d2cbfd69cd41
peterhoward42 <peter@keel.xyz>
2023-02-08 17:21:59 +0000

    fix: make prettier

--- integration/testdata/custom_function_api/example.test.ts
+++ integration/testdata/custom_function_api/example.test.ts
@@ -3,7 +3,7 @@ import { test, expect } from "vitest";
 
 test("make sure api.fetch works", async () => {
   const fetchedThing = await actions.getFetchedThing({});
-  expect(fetchedThing).not.toBeNull()
-  const body = fetchedThing?.fetchedBody
-  expect(body?.startsWith("<!doctype html")).toBe(true)
+  expect(fetchedThing).not.toBeNull();
+  const body = fetchedThing?.fetchedBody;
+  expect(body?.startsWith("<!doctype html")).toBe(true);
 });
