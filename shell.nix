53b5e7797604e639a3052e295cbefac154d672ac
Hugo Sousa <gi.ciberon@gmail.com>
2023-02-08 18:52:59 +0100

    chore: add nixfmt and format shell.nix

--- shell.nix
+++ shell.nix
@@ -1,5 +1,6 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/0218941ea68b4c625533bead7bbb94ccce52dceb.tar.gz") {}
-}:
+{ pkgs ? import (fetchTarball
+  "https://github.com/NixOS/nixpkgs/archive/0218941ea68b4c625533bead7bbb94ccce52dceb.tar.gz")
+  { } }:
 
 pkgs.mkShell {
   buildInputs = [
@@ -9,5 +10,6 @@ pkgs.mkShell {
     pkgs.nodePackages_latest.pnpm
     pkgs.protobuf
     pkgs.protoc-gen-go
+    pkgs.nixfmt
   ];
 }
