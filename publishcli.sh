21507002173c902fce1f445be34c9e1f4543d496
Adam Bull <adam@keel.xyz>
2023-02-13 16:49:56 +0000

    feat: compress binary with upx (#774)

--- publishcli.sh
+++ publishcli.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # Compiles the keel cli for OS X + Apple silicon
-GOOS=darwin GOARCH=arm64 go build -ldflags="-X 'github.com/teamkeel/keel/cmd.Version=$1'" -o ./dist/keel cmd/keel/main.go
+GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w -X 'github.com/teamkeel/keel/cmd.Version=$1'" -o ./dist/keel cmd/keel/main.go
+
+# compress the binary using upx
+sudo apt-get install -y upx
+upx --brute dist/keel
