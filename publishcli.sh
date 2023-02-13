5d4adb45fcdf0ec4820db5bf7a4c67e6ea0175b3
Adam Bull <adam@keel.xyz>
2023-02-13 15:22:16 +0000

    feat: chmod binary

--- publishcli.sh
+++ publishcli.sh
@@ -2,3 +2,5 @@
 
 # Compiles the keel cli for OS X + Apple silicon
 GOOS=darwin GOARCH=arm64 go build -ldflags="-X 'github.com/teamkeel/keel/cmd.Version=$1'" -o ./dist/keel cmd/keel/main.go
+
+chmod +x ./dist/keel
