08fc2371a487d6341e6faa599a672c2add89be77
Adam Bull <adam@keel.xyz>
2023-02-16 10:53:19 +0000

    chore: tests

--- integration/testdata/operation_list_pagination/main.test.ts
+++ integration/testdata/operation_list_pagination/main.test.ts
@@ -1,18 +1,41 @@
 import { test, expect, beforeEach } from "vitest";
+import { Post } from "@teamkeel/sdk";
 import { actions, resetDatabase, models } from "@teamkeel/testing";
 
 beforeEach(resetDatabase);
 
-test("pagination simple", async () => {
-  const posts = await Promise.all(
-    Array.from(Array(20)).map((_, i) =>
-      models.post.create({ title: `Post ${i}` })
-    )
-  );
+test("pagination - before", async () => {
+  const posts: Post[] = [];
+  posts.push(await models.post.create({ id: "1", title: "Post 1" }));
+  posts.push(await models.post.create({ id: "2", title: "Post 2" }));
+  posts.push(await models.post.create({ id: "3", title: "Post 3" }));
+  posts.push(await models.post.create({ id: "4", title: "Post 4" }));
+  posts.push(await models.post.create({ id: "5", title: "Post 5" }));
+  posts.push(await models.post.create({ id: "6", title: "Post 6" }));
+
+  const cursor = posts[3].id;
 
   const { results } = await actions.listPosts({
-    after: posts[10].id,
+    before: cursor,
   });
 
-  expect(results.length).toEqual(10);
+  expect(results.length).toEqual(3);
+});
+
+test("pagination - after", async () => {
+  const posts: Post[] = [];
+  posts.push(await models.post.create({ id: "1", title: "Post 1" }));
+  posts.push(await models.post.create({ id: "2", title: "Post 2" }));
+  posts.push(await models.post.create({ id: "3", title: "Post 3" }));
+  posts.push(await models.post.create({ id: "4", title: "Post 4" }));
+  posts.push(await models.post.create({ id: "5", title: "Post 5" }));
+  posts.push(await models.post.create({ id: "6", title: "Post 6" }));
+
+  const cursor = posts[2].id;
+
+  const { results } = await actions.listPosts({
+    after: cursor,
+  });
+
+  expect(results.length).toEqual(3);
 });
