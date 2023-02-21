e7175a4da484432421bce8204575da5fe13473a1
Adam Bull <adam@keel.xyz>
2023-02-21 17:30:29 +0000

    feat: set chmod permissions on binary

--- publishcli.sh
+++ publishcli.sh
@@ -2,3 +2,7 @@
 
 # Compiles the keel cli for OS X + Apple silicon
 GOOS=darwin GOARCH=arm64 go build -ldflags="-X 'github.com/teamkeel/keel/cmd.Version=$1'" -o ./dist/keel cmd/keel/main.go
+
+# Set the keel binary as an executable in the git index
+# https://git-scm.com/docs/git-update-index#Documentation/git-update-index.txt---chmod-x
+git add --chmod=+x dist/keel
