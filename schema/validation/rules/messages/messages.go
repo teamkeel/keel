750496ef9ee53bdb5639944d50e662738db70f63
Jon Bretman <jon.bretman@gmail.com>
2023-03-13 20:26:26 +0000

    refactor: update NamingRule to handle messages and tidy up tests

--- schema/validation/rules/messages/messages.go
+++ schema/validation/rules/messages/messages.go
@@ -3,30 +3,10 @@ package messages
 import (
 	"fmt"
 
-	"github.com/iancoleman/strcase"
 	"github.com/teamkeel/keel/schema/parser"
 	"github.com/teamkeel/keel/schema/query"
 	"github.com/teamkeel/keel/schema/validation/errorhandling"
 )
-
-func MessageNamesRule(asts []*parser.AST) (errs errorhandling.ValidationErrors) {
-	for _, message := range query.Messages(asts) {
-		if message.Name.Value != strcase.ToCamel(message.Name.Value) {
-			errs.AppendError(
-				errorhandling.NewValidationErrorWithDetails(
-					errorhandling.NamingError,
-					errorhandling.ErrorDetails{
-						Message: fmt.Sprintf("'%s' is not in upper camel case", message.Name.Value),
-						Hint:    fmt.Sprintf("Use '%s' instead", strcase.ToCamel(message.Name.Value)),
-					},
-					message.Name,
-				),
-			)
-		}
-	}
-
-	return
-}
 
 func UniqueMessageNamesRule(asts []*parser.AST) (errs errorhandling.ValidationErrors) {
 	seenMessageNames := map[string]bool{}
