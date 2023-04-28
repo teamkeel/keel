723fa5fa1321a140478acff8e02ce17e5162d531
Adam Bull <adam@keel.xyz>
2023-04-28 14:01:38 +0100

    chore: undo console.log

--- integration/testdata/with_custom_function/functions/createPerson.ts
+++ integration/testdata/with_custom_function/functions/createPerson.ts
@@ -2,25 +2,6 @@ import { CreatePerson } from "@teamkeel/sdk";
 
 export default CreatePerson(async (inputs, api, ctx) => {
   api.permissions.allow();
-  console.log(
-    JSON.stringify(
-      {
-        hello: "1",
-        arr: [
-          {
-            dkk: "1",
-          },
-          {
-            ddkdjdj: "1",
-          },
-        ],
-      },
-      null,
-      2
-    )
-  );
-  const result = await api.models.person.create(inputs);
 
-  console.log(result);
-  return result;
+  return await api.models.person.create(inputs);
 });
