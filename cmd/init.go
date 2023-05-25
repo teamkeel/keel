9c837fa1d32ca45a9c17f0e431ea88b4020bfb56
Adam Bull <adam@keel.xyz>
2023-05-25 21:30:47 +0100

    feat: init cmd as standalone tea program (#986)
    * feat: standalone init
    
    * chore: fix dir arg length bug

--- cmd/init.go
+++ cmd/init.go
@@ -3,25 +3,39 @@ package cmd
 import (
 	"os"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
 	"github.com/teamkeel/keel/cmd/program"
 )
 
 var initCmd = &cobra.Command{
 	Use:   "init",
-	Args:  cobra.MaximumNArgs(1),
+	Args:  cobra.RangeArgs(0, 1),
 	Short: "Initializes a new Keel project",
 	Run: func(cmd *cobra.Command, args []string) {
-		dir, _ := os.Getwd()
+		cwd, err := os.Getwd()
+		if err != nil {
+			panic(err)
+		}
+
+		dir := cwd
 
 		if len(args) > 0 {
 			dir = args[0]
 		}
 
-		program.Run(&program.Model{
-			Mode:       program.ModeInit,
+		model := &program.InitModel{
 			ProjectDir: dir,
-		})
+		}
+
+		_, err = tea.NewProgram(model).Run()
+		if err != nil {
+			panic(err)
+		}
+
+		if model.Err != nil {
+			os.Exit(1)
+		}
 	},
 }
 
