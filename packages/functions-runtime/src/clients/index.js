43eb292ba3196bc0511beae2213e1fb5ef251f5c
Adam Bull <adam@keel.xyz>
2023-06-02 11:26:00 +0100

    build: switching logic

---
+++ packages/functions-runtime/src/clients/index.js
@@ -0,0 +1,6 @@
+const KNOWN_CLIENTS = {
+  KYSELY: 'kysely',
+  PRISMA: 'prisma'
+}
+
+module.exports.KNOWN_CLIENTS = KNOWN_CLIENTS
--- packages/functions-runtime/src/database.js
+++ packages/functions-runtime/src/database.js
@@ -1,4 +1,4 @@
-const { Kysely } = require("kysely");
+const { KNOWN_CLIENTS } = require('./clients')
 const { AsyncLocalStorage } = require("async_hooks");
 const { PROTO_ACTION_TYPES } = require("./consts");
 const { getDatabase: getKysely } =  require("./clients/kysely")
@@ -7,7 +7,7 @@ const { getDatabase: getKysely } =  require("./clients/kysely")
 // and sets the transaction in the AsyncLocalStorage so consumers further
 // down the hierarchy can access the current transaction.
 // For read type operations such as list & get, no transaction is used
-async function withTransaction({ actionType, orm = "kysely" }, cb) {
+async function withTransaction({ actionType, orm = KNOWN_CLIENTS.KYSELY }, cb) {
   const db = getDatabase(orm)
 
   let requiresTransaction = true;
@@ -19,11 +19,16 @@ async function withTransaction({ actionType, orm = "kysely" }, cb) {
   }
 
   if (requiresTransaction) {
-    return db.transaction().execute(async (transaction) => {
-      return dbInstance.run(transaction, async () => {
-        return cb({ transaction, db });
-      });
-    });
+    switch(orm) {
+      case KNOWN_CLIENTS.KYSELY:
+        return db.transaction().execute(async (transaction) => {
+          return dbInstance.run(transaction, async () => {
+            return cb({ transaction, db });
+          });
+        });
+      case KNOWN_CLIENTS.PRISMA:
+    }
+    
   }
   return dbInstance.run(db, async () => {
     return cb({ db, transaction: null });
@@ -37,7 +42,7 @@ const dbInstance = new AsyncLocalStorage();
 
 // getDatabase will first check for an instance of Kysely in AsyncLocalStorage,
 // otherwise it will create a new instance and reuse it..
-function getDatabase(orm= 'kysely') {
+function getDatabase(orm = KNOWN_CLIENTS.KYSELY) {
   let fromStore = dbInstance.getStore();
   if (fromStore) {
     return fromStore;
@@ -49,8 +54,10 @@ function getDatabase(orm= 'kysely') {
 
 
   switch(orm) {
-    case 'kysely':
+    case KNOWN_CLIENTS.KYSELY:
       db = getKysely()
+    case KNOWN_CLIENTS.PRISMA:
+      // todo
     default:
 
   }
