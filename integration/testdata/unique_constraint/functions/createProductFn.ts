c9dd61ad58b4dcc6422a8438d07ccd2b8f941f69
Dave New <sanodn@gmail.com>
2023-06-19 16:04:13 +0200

    chore: prettier

--- integration/testdata/unique_constraint/functions/createProductFn.ts
+++ integration/testdata/unique_constraint/functions/createProductFn.ts
@@ -1,7 +1,6 @@
-import { CreateProductFn, models } from '@teamkeel/sdk';
+import { CreateProductFn, models } from "@teamkeel/sdk";
 
 export default CreateProductFn(async (ctx, inputs) => {
-	const product = await models.product.create(inputs);
-	return product;
+  const product = await models.product.create(inputs);
+  return product;
 });
-	
--- integration/testdata/unique_constraint/functions/updateProductFn.ts
+++ integration/testdata/unique_constraint/functions/updateProductFn.ts
@@ -1,7 +1,6 @@
-import { UpdateProductFn, models } from '@teamkeel/sdk';
+import { UpdateProductFn, models } from "@teamkeel/sdk";
 
 export default UpdateProductFn(async (ctx, inputs) => {
-	const product = await models.product.update(inputs.where, inputs.values!);
-	return product;
+  const product = await models.product.update(inputs.where, inputs.values!);
+  return product;
 });
-	
