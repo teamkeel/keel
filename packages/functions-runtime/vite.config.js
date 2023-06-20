108816e2a5767ea1754156d96b9087dfc344ed8a
Adam Bull <adamjamesbull@googlemail.com>
2023-06-20 10:12:39 +0100

    chore: prettier

--- packages/functions-runtime/vite.config.js
+++ packages/functions-runtime/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv } from "vite";
 
 export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '')};
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), "") };
 
   return defineConfig({});
-}
+};
