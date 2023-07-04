71783801194c0505c77cdc9bb0e9370e19c313f7
Emily Toop <12613048+fluffyemily@users.noreply.github.com>
2023-07-04 10:49:54 +0100

    BLD-483 Extend validation to take into account `@relation` attribute (#1031)
    Check if a relation attribute exists that disambiguates the relationship in 1:1 and m:m validation

--- schema/validation/rules/relationships/relationships.go
+++ schema/validation/rules/relationships/relationships.go
@@ -180,6 +180,16 @@ func InvalidOneToOneRelationshipRule(asts []*parser.AST) (errs errorhandling.Val
 					continue
 				}
 
+				// check to see if a relation is defined on this attribute that
+				// disambiguates the reference.
+				relation := query.FieldGetAttribute(field, parser.AttributeRelation)
+				if relation != nil {
+					relationValue, _ := relation.Arguments[0].Expression.ToValue()
+					if relationValue.ToString() != otherField.Name.Value {
+						continue
+					}
+				}
+
 				errs.Append(
 					errorhandling.ErrorInvalidOneToOneRelationship,
 					map[string]string{
