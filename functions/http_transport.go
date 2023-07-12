aae2512c23d2003cfd4f540feb3856d16c204c6e
Dave New <sanodn@gmail.com>
2023-07-12 16:01:41 +0200

    chore: moving func type

--- functions/http_transport.go
+++ functions/http_transport.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	urlPath "net/url"
 
 	"github.com/pkg/errors"
 )
@@ -17,24 +16,23 @@ type HttpFunctionsClient struct {
 }
 
 func NewHttpTransport(url string) Transport {
-	return func(ctx context.Context, req *FunctionsRuntimeRequest, functionType FunctionType) (*FunctionsRuntimeResponse, error) {
+	return func(ctx context.Context, req *FunctionsRuntimeRequest) (*FunctionsRuntimeResponse, error) {
 		b, err := json.Marshal(req)
 		if err != nil {
 			return nil, err
 		}
 
-		targetUrl, err := urlPath.JoinPath(url, string(functionType))
 		if err != nil {
 			return nil, err
 		}
 
-		resp, err := http.Post(targetUrl, "application/json", bytes.NewReader(b))
+		resp, err := http.Post(url, "application/json", bytes.NewReader(b))
 		if err != nil {
 			return nil, err
 		}
 
 		if resp.StatusCode != http.StatusOK {
-			return nil, fmt.Errorf("node server responded with status code '%d' for '%s'", resp.StatusCode, targetUrl)
+			return nil, fmt.Errorf("node server responded with status code '%d' for '%s'", resp.StatusCode, url)
 		}
 
 		b, err = io.ReadAll(resp.Body)
