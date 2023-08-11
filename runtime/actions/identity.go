6e1ac5075e47aad731c4e565414812bad03c0d08
dblooman <david.blooman@gmail.com>
2023-08-11 17:07:23 +0100

    fix: rename to use issuer

--- runtime/actions/identity.go
+++ runtime/actions/identity.go
@@ -94,11 +94,11 @@ func CreateIdentity(ctx context.Context, schema *proto.Schema, email string, pas
 	return mapToIdentity(result)
 }
 
-func CreateExternalIdentity(ctx context.Context, schema *proto.Schema, externalId string, issuer string, jwt string) (*runtimectx.Identity, error) {
+func CreateExternalIdentity(ctx context.Context, schema *proto.Schema, externalId string, iss string, jwt string) (*runtimectx.Identity, error) {
 	ctx, span := tracer.Start(ctx, "Create external identity")
 	defer span.End()
 
-	issuer = strings.TrimSuffix(issuer, "/")
+	issuer := strings.TrimSuffix(iss, "/")
 
 	span.SetAttributes(attribute.String("externalId", externalId))
 	span.SetAttributes(attribute.String("issuer", issuer))
