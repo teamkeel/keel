dac9b0ec2872e3590f809f51085bad1b8fa30ec7
David Blooman <david.blooman@gmail.com>
2023-08-11 23:37:49 +0100

    fix: function for handling auth

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -289,3 +289,32 @@ func HandleAuthorizationHeader(ctx context.Context, schema *proto.Schema, header
 
 	return identity, nil
 }
+
+func HandleBearerToken(ctx context.Context, schema *proto.Schema, token string) (*runtimectx.Identity, error) {
+	subject, issuer, err := actions.ValidateBearerToken(ctx, token)
+	if err != nil {
+		return nil, err
+	}
+
+	// Check that identity actually does exist as it could
+	// have been deleted after the bearer token was generated.
+	var identity *runtimectx.Identity
+	if issuer == "keel" || issuer == "" {
+		identity, err = actions.FindIdentityById(ctx, schema, subject)
+	} else {
+		identity, err = actions.FindIdentityByExternalId(ctx, schema, subject, issuer)
+		if identity == nil {
+			identity, err = actions.CreateExternalIdentity(ctx, schema, subject, issuer, token)
+		}
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if identity == nil {
+		return nil, actions.ErrIdentityNotFound
+	}
+
+	return identity, nil
+}
