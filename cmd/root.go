2e2ee0d179e4ad7bfbb17b46775c80b3ae68ba60
Shedrack akintayo <akintayoshedrack@gmail.com>
2023-10-19 07:32:51 +0100

    fix: using --version or -v flags (#1237)

--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,11 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/teamkeel/keel/runtime"
 )
 
 var enabledDebugFlags = "true"
@@ -16,16 +18,24 @@ var (
 	flagPrivateKeyPath   string
 	flagPattern          string
 	flagTracing          bool
+	flagVersion          bool
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "keel",
 	Short: "The Keel CLI",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if flagVersion {
+			fmt.Printf("v%s\n", runtime.GetVersion())
+			os.Exit(0)
+		}
+		return cmd.Help()
+	},
 }
 
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 		os.Exit(1)
 	}
 }
@@ -37,4 +47,5 @@ func init() {
 	}
 
 	rootCmd.PersistentFlags().StringVarP(&flagProjectDir, "dir", "d", workingDir, "directory containing a Keel project")
+	rootCmd.PersistentFlags().BoolVarP(&flagVersion, "version", "v", false, "Print the Keel CLI version")
 }
