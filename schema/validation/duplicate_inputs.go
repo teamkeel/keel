fb2ca31e16c002ec5523acc2836f2c2799867fbe
Dave New <sanodn@gmail.com>
2023-11-01 16:22:25 +0200

    fix: duplicate inputs permitted across query and write inputs (#1275)

--- schema/validation/duplicate_inputs.go
+++ schema/validation/duplicate_inputs.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/iancoleman/strcase"
 	"github.com/samber/lo"
 	"github.com/teamkeel/keel/schema/parser"
 	"github.com/teamkeel/keel/schema/validation/errorhandling"
@@ -12,11 +11,15 @@ import (
 
 // DuplicateInputsRule checks that input names are not duplicated for an action.
 func DuplicateInputsRule(asts []*parser.AST, errs *errorhandling.ValidationErrors) Visitor {
-	var names []string
+	var queryInputs []string
+	var writeInputs []string
+	var action *parser.ActionNode
 
 	return Visitor{
 		EnterAction: func(n *parser.ActionNode) {
-			names = []string{}
+			queryInputs = []string{}
+			writeInputs = []string{}
+			action = n
 		},
 		EnterActionInput: func(n *parser.ActionInputNode) {
 			var input string
@@ -32,23 +35,29 @@ func DuplicateInputsRule(asts []*parser.AST, errs *errorhandling.ValidationError
 				input = strings.Join(fragments, ".")
 			}
 
-			// This is a hacky way of checking if this is a message, which we'll skip from the validation.
-			// Otherwise we could run into a duplicate input validation error on this: write writeFn(Any) returns (Any)
-			// I would really like to differentiate between Input and Output nodes on the AST, as it would make validation much easier (and the rules are different).
-			// My original proposed PR on the matter: https://github.com/teamkeel/keel/pull/1016/files#diff-f880f21e2ba759b058ddc06776b9962736266b247d4b38e5d703a0802ca08d6d
-			if strcase.ToCamel(input) == input {
-				return
-			}
-
-			if lo.Contains(names, input) {
-				errs.AppendError(errorhandling.NewValidationErrorWithDetails(
-					errorhandling.NamingError,
-					errorhandling.ErrorDetails{
-						Message: fmt.Sprintf("'%s' has already been defined as an input on this action", input),
-					},
-					n))
-			} else {
-				names = append(names, input)
+			switch {
+			case lo.Contains(action.Inputs, n):
+				if lo.Contains(queryInputs, input) {
+					errs.AppendError(errorhandling.NewValidationErrorWithDetails(
+						errorhandling.NamingError,
+						errorhandling.ErrorDetails{
+							Message: fmt.Sprintf("'%s' has already been defined as a query input on this action", input),
+						},
+						n))
+				} else {
+					queryInputs = append(queryInputs, input)
+				}
+			case lo.Contains(action.With, n):
+				if lo.Contains(writeInputs, input) {
+					errs.AppendError(errorhandling.NewValidationErrorWithDetails(
+						errorhandling.NamingError,
+						errorhandling.ErrorDetails{
+							Message: fmt.Sprintf("'%s' has already been defined as a write input on this action", input),
+						},
+						n))
+				} else {
+					writeInputs = append(writeInputs, input)
+				}
 			}
 		},
 	}
