3a97e0433c95222ed96f4b2024ea3fb9adf8bbce
Dave New <sanodn@gmail.com>
2023-11-09 03:40:19 +0200

    fix: validation for unsupported job input types

--- schema/validation/jobs.go
+++ schema/validation/jobs.go
@@ -41,6 +41,16 @@ func Jobs(asts []*parser.AST, errs *errorhandling.ValidationErrors) Visitor {
 			}
 		},
 		EnterJobInput: func(input *parser.JobInputNode) {
+			if !parser.IsBuiltInFieldType(input.Type.Value) {
+				errs.AppendError(errorhandling.NewValidationErrorWithDetails(
+					errorhandling.JobDefinitionError,
+					errorhandling.ErrorDetails{
+						Message: fmt.Sprintf("Job input '%s' is defined with unsupported type %s", input.Name.Value, input.Type.Value),
+					},
+					input.Name,
+				))
+			}
+
 			if lo.Contains(jobInputs, input.Name.Value) {
 				errs.AppendError(errorhandling.NewValidationErrorWithDetails(
 					errorhandling.DuplicateDefinitionError,
