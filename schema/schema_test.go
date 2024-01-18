39aa80db9210b097fa20cbe1a965337777634b14
Dave New <sanodn@gmail.com>
2024-01-18 17:04:14 +0200

    fix: validation

--- schema/schema_test.go
+++ schema/schema_test.go
@@ -67,6 +67,10 @@ func TestValidation(t *testing.T) {
 	require.NoError(t, err)
 
 	for _, testCase := range testCases {
+		if testCase.Name() != "api_invalid_action_names.keel" {
+			continue
+		}
+
 		if testCase.IsDir() {
 			t.Errorf("errors test data directory should only contain keel schema files - directory found: %s", testCase.Name())
 			continue
