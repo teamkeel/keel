169c3afc301a31784077efebe8c766df559141b3
Radu Gruia <radu@keel.xyz>
2024-04-03 11:27:21 +0100

    fix: split numbers when converting between camelCase and snake_case (#1451)

--- packages/functions-runtime/src/casing.js
+++ packages/functions-runtime/src/casing.js
@@ -3,7 +3,16 @@ const { snakeCase, camelCase } = require("change-case");
 function camelCaseObject(obj = {}) {
   const r = {};
   for (const key of Object.keys(obj)) {
-    r[camelCase(key)] = obj[key];
+    r[
+      camelCase(key, {
+        transform: camelCaseTransform,
+        splitRegexp: [
+          /([a-z0-9])([A-Z])/g,
+          /([A-Z])([A-Z][a-z])/g,
+          /([a-zA-Z])([0-9])/g,
+        ],
+      })
+    ] = obj[key];
   }
   return r;
 }
@@ -11,7 +20,15 @@ function camelCaseObject(obj = {}) {
 function snakeCaseObject(obj) {
   const r = {};
   for (const key of Object.keys(obj)) {
-    r[snakeCase(key)] = obj[key];
+    r[
+      snakeCase(key, {
+        splitRegexp: [
+          /([a-z0-9])([A-Z])/g,
+          /([A-Z])([A-Z][a-z])/g,
+          /([a-zA-Z])([0-9])/g,
+        ],
+      })
+    ] = obj[key];
   }
   return r;
 }
@@ -19,6 +36,13 @@ function snakeCaseObject(obj) {
 function upperCamelCase(s) {
   s = camelCase(s);
   return s[0].toUpperCase() + s.substring(1);
+}
+
+function camelCaseTransform(input, index) {
+  if (index === 0) return input.toLowerCase();
+  const firstChar = input.charAt(0);
+  const lowerChars = input.substr(1).toLowerCase();
+  return `${firstChar.toUpperCase()}${lowerChars}`;
 }
 
 module.exports = {
