48f4074272c6d8985483bd28f1ebb06c8e43af0f
Dave New <sanodn@gmail.com>
2024-04-30 17:47:02 +0200

    feat: vector type work in prog

---
+++ integration/testdata/vectors/tests.test.ts
@@ -0,0 +1,8 @@
+import { actions, resetDatabase, models } from "@teamkeel/testing";
+import { test, expect, beforeEach } from "vitest";
+
+beforeEach(resetDatabase);
+
+test("vectors", async () => {
+
+});
