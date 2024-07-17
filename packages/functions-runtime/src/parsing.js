7729c4931205148a85becd9564244bcf751f69c6
Radu Gruia <radu@keel.xyz>
2024-07-17 16:20:24 +0100

    fix: docs

--- packages/functions-runtime/src/parsing.js
+++ packages/functions-runtime/src/parsing.js
@@ -1,5 +1,9 @@
 const { InlineFile } = require("./InlineFile");
 
+// parseParams takes a set of inputs and creates objects for the ones that are of a complex type.
+//
+// inputs that are objects and contain a "__typename" field are resolved to instances of the complex type
+// they represent. At the moment, the only supported type is `InlineFile`
 function parseParams(inputs) {
   if (inputs != null) {
     Object.keys(inputs).forEach((i) => {
