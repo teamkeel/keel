7730c4561bf74b1066636a6897af4fdf5cf30b41
Radu Gruia <radu@keel.xyz>
2024-07-31 10:17:55 +0100

    feat: store InlineFiles (#1534)

--- packages/functions-runtime/src/InlineFile.js
+++ packages/functions-runtime/src/InlineFile.js
@@ -1,3 +1,9 @@
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { fromEnv } = require("@aws-sdk/credential-providers");
+const { useDatabase } = require("./database");
+const { DatabaseError } = require("./errors");
+const KSUID = require("ksuid");
+
 class InlineFile {
   constructor(filename, contentType, size, url) {
     this.filename = filename;
@@ -41,14 +47,65 @@ class InlineFile {
 
     if (this._dataURL) {
       var data = this._dataURL.split(",")[1];
-      var byteString = Buffer.from(data, "base64");
-      return new Blob([byteString], { type: this.contentType });
+      return Buffer.from(data, "base64");
     }
   }
 
-  store() {
-    //TODO: actually store and generate a key
-    this.key = uuidv4();
+  async store() {
+    const content = this.read();
+    this.key = KSUID.randomSync().string;
+
+    if (isS3Storage()) {
+      // Initialize the S3 client
+      const s3Client = new S3Client({
+        credentials: fromEnv(),
+        region: process.env.KEEL_REGION,
+      });
+
+      const params = {
+        Bucket: process.env.KEEL_FILES_BUCKET_NAME,
+        Key: key,
+        Body: content,
+        ContentType: this.contentType,
+      };
+
+      const command = new PutObjectCommand(params);
+      try {
+        const result = await s3Client.send(command);
+        console.log(`File uploaded successfully. ETag: ${result.ETag}`);
+        return {
+          key: this.key,
+          size: this.size,
+          filename: this.filename,
+          contentType: this.contentType,
+        };
+      } catch (error) {
+        console.error("Error uploading file:", error);
+        throw error;
+      }
+    }
+
+    // default to db storage
+    const db = useDatabase();
+
+    try {
+      let query = db.insertInto("keel_storage").values({
+        id: this.key,
+        filename: this.filename,
+        content_type: this.contentType,
+        data: content,
+      });
+
+      const created = await query.returningAll().executeTakeFirstOrThrow();
+      return {
+        key: this.key,
+        size: this.size,
+        filename: this.filename,
+        contentType: this.contentType,
+      };
+    } catch (e) {
+      throw new DatabaseError(e);
+    }
   }
 
   toJSON() {
@@ -66,3 +123,7 @@ class InlineFile {
 module.exports = {
   InlineFile,
 };
+
+function isS3Storage() {
+  return "KEEL_FILES_BUCKET_NAME" in process.env;
+}
