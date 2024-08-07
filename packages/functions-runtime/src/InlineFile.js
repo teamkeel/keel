5665b7360cfefe2d843846345ce5f65c47d3a7a4
Radu Gruia <radu@keel.xyz>
2024-08-07 15:17:44 +0100

    feat: inline files read (#1545)

--- packages/functions-runtime/src/InlineFile.js
+++ packages/functions-runtime/src/InlineFile.js
@@ -1,15 +1,20 @@
-const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} = require("@aws-sdk/client-s3");
 const { fromEnv } = require("@aws-sdk/credential-providers");
 const { useDatabase } = require("./database");
 const { DatabaseError } = require("./errors");
 const KSUID = require("ksuid");
 
 class InlineFile {
-  constructor(filename, contentType, size, url) {
+  constructor(filename, contentType, size, url, key) {
     this.filename = filename;
     this.contentType = contentType;
     this.size = size;
     this.url = url;
+    this.key = key;
   }
 
   // Create an InlineFile instance from a given json object.
@@ -20,7 +25,13 @@ class InlineFile {
       return file;
     }
 
-    return new InlineFile(obj.filename, obj.contentType, obj.size, obj.url);
+    return new InlineFile(
+      obj.filename,
+      obj.contentType,
+      obj.size,
+      obj.url,
+      obj.key
+    );
   }
 
   // Create an InlineFile instance from a given dataURL
@@ -40,19 +51,49 @@ class InlineFile {
 
   // Read the contents of the file. If URL is set, it will be read from the remote storage, otherwise, if dataURL is set
   // on the instance, it will return a blob with the file contents
-  read() {
-    if (this.url) {
-      // TODO: read from store
-    }
-
+  async read() {
     if (this._dataURL) {
       var data = this._dataURL.split(",")[1];
       return Buffer.from(data, "base64");
     }
+
+    // if we don't have a key nor a dataURL, this inline file has no data
+    if (!this.key) {
+      throw new Error("invalid file data");
+    }
+
+    if (isS3Storage()) {
+      const s3Client = new S3Client({
+        credentials: fromEnv(),
+        region: process.env.KEEL_REGION,
+      });
+
+      const params = {
+        Bucket: process.env.KEEL_FILES_BUCKET_NAME,
+        Key: "files/" + this.key,
+      };
+      const command = new GetObjectCommand(params);
+      const response = await s3Client.send(command);
+      return response.Body.transformToString();
+    }
+
+    // default to db storage
+    const db = useDatabase();
+
+    try {
+      let query = db.selectFrom("keel_storage").select("data").where({
+        id: this.key,
+      });
+
+      const row = await query.executeTakeFirstOrThrow();
+      return row.data;
+    } catch (e) {
+      throw new DatabaseError(e);
+    }
   }
 
-  async store() {
-    const content = this.read();
+  async store(expires = null) {
+    const content = await this.read();
     this.key = KSUID.randomSync().string;
 
     if (isS3Storage()) {
@@ -63,7 +104,7 @@ class InlineFile {
 
       const params = {
         Bucket: process.env.KEEL_FILES_BUCKET_NAME,
-        Key: this.key,
+        Key: "files/" + this.key,
         Body: content,
         ContentType: this.contentType,
         Metadata: {
@@ -71,10 +112,14 @@ class InlineFile {
         },
       };
 
+      if (expires) {
+        params.Expires = expires;
+      }
+
       const command = new PutObjectCommand(params);
       try {
-        const result = await s3Client.send(command);
-        console.log(`File uploaded successfully. ETag: ${result.ETag}`);
+        await s3Client.send(command);
+
         return {
           key: this.key,
           size: this.size,
