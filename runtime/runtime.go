f3ab6b83497275271d833734ed627ae756287840
Dave New <sanodn@gmail.com>
2024-09-04 10:01:24 +0200

    chore: check for jobs without inputs

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -209,11 +209,13 @@ func (handler JobHandler) RunJob(ctx context.Context, jobName string, input map[
 		}
 	}
 
-	message := scope.Schema.FindMessage(job.InputMessageName)
-
-	input, err := actions.TransformInputs(handler.schema, message, input, true)
-	if err != nil {
-		return err
+	var err error
+	if job.InputMessageName != "" {
+		message := scope.Schema.FindMessage(job.InputMessageName)
+		input, err = actions.TransformInputs(handler.schema, message, input, true)
+		if err != nil {
+			return err
+		}
 	}
 
 	err = functions.CallJob(
