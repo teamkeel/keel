999da98df33aae551dc3f0922130e2f7a5db1d25
Dave New <sanodn@gmail.com>
2024-12-20 10:44:42 +0200

    chore: remove comment

--- schema/schema_test.go
+++ schema/schema_test.go
@@ -72,10 +72,6 @@ func TestValidation(t *testing.T) {
 	require.NoError(t, err)
 
 	for _, tc := range testCases {
-		// if tc.Name() != "attribute_computed_expression.keel" {
-		// 	continue
-		// }
-
 		testCase := tc
 		if testCase.IsDir() {
 			t.Errorf("errors test data directory should only contain keel schema files - directory found: %s", testCase.Name())
