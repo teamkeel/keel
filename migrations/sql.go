7afcd6db43e58f2340a499c63422dfa5b1f288a3
Dave New <sanodn@gmail.com>
2025-02-02 10:01:55 +0200

    fix: required computed fields and deferred null constraint (#1716)

--- migrations/sql.go
+++ migrations/sql.go
@@ -214,7 +214,11 @@ func alterColumnStmt(modelName string, field *proto.Field, column *ColumnRow) (s
 		if field.Optional && column.NotNull {
 			change = "DROP NOT NULL"
 		} else {
-			change = "SET NOT NULL"
+			// If computed, then we don't set the NOT NULL constraint yet
+			// This is because we may still need to populate existing rows
+			if field.ComputedExpression == nil {
+				change = "SET NOT NULL"
+			}
 
 			// Update all existing rows to the default value if they are null
 			if field.DefaultValue != nil {
@@ -226,7 +230,9 @@ func alterColumnStmt(modelName string, field *proto.Field, column *ColumnRow) (s
 				stmts = append(stmts, update)
 			}
 		}
-		stmts = append(stmts, fmt.Sprintf("%s %s;", alterColumnStmtPrefix, change))
+		if change != "" {
+			stmts = append(stmts, fmt.Sprintf("%s %s;", alterColumnStmtPrefix, change))
+		}
 	}
 
 	return strings.Join(stmts, "\n"), nil
@@ -329,7 +335,9 @@ func fieldDefinition(field *proto.Field) (string, error) {
 
 	output := fmt.Sprintf("%s %s", columnName, fieldType)
 
-	if !field.Optional {
+	// If computed, then we don't set the NOT NULL constraint yet
+	// This is because we may still need to populate existing rows
+	if !field.Optional && field.ComputedExpression == nil {
 		output += " NOT NULL"
 	}
 
