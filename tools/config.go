2ed815c8c8b6d04d5a6dced2f1064f76282a84f2
Radu Gruia <radu@keel.xyz>
2025-03-04 13:17:43 +0100

    feat: support configuring resposnes

--- tools/config.go
+++ tools/config.go
@@ -32,45 +32,45 @@ func (cfgs ToolConfigs) getIDs() []string {
 }
 
 type ToolConfig struct {
-	ID           string       `json:"id,omitempty"`
-	ActionName   string       `json:"action_name,omitempty"`
-	Name         string       `json:"name,omitempty"`
-	Icon         string       `json:"icon,omitempty"`
-	Title        string       `json:"title,omitempty"`
-	HelpText     string       `json:"help_text,omitempty"`
-	Capabilities Capabilities `json:"capabilities,omitempty"`
-	EntitySingle string       `json:"entity_single,omitempty"`
-	EntityPlural string       `json:"entity_plural,omitempty"`
-	Inputs       InputConfigs `json:"inputs,omitempty"`
-	// TODO: RelatedActions       []*ToolLink
+	ID           string          `json:"id,omitempty"`
+	ActionName   string          `json:"action_name,omitempty"`
+	Name         string          `json:"name,omitempty"`
+	Icon         *string         `json:"icon,omitempty"`
+	Title        *string         `json:"title,omitempty"`
+	HelpText     *string         `json:"help_text,omitempty"`
+	Capabilities Capabilities    `json:"capabilities,omitempty"`
+	EntitySingle *string         `json:"entity_single,omitempty"`
+	EntityPlural *string         `json:"entity_plural,omitempty"`
+	Inputs       InputConfigs    `json:"inputs,omitempty"`
+	Response     ResponseConfigs `json:"response,omitempty"`
 	// TODO: ExternalLinks        ExternalLinks
+	// TODO: RelatedActions       []*ToolLink
 	// TODO: EntryActivityActions []*ToolLink
-	// TODO: EmbeddedTools        ToolGroups
 	// TODO: GetEntryAction       *ToolLink
 	// TODO: CreateEntryAction    *ToolLink
+	// TODO: EmbeddedTools        ToolGroups
 	// TODO: DisplayLayout        *DisplayLayout
 	// TODO: Sections             Sections
-	// TODO: Response
 }
 
 func (cfg *ToolConfig) applyOn(tool *toolsproto.ActionConfig) {
 	if cfg.Name != "" {
 		tool.Name = cfg.Name
 	}
-	if cfg.Title != "" {
-		tool.Title = &toolsproto.StringTemplate{Template: cfg.Title}
+	if cfg.Title != nil {
+		tool.Title = &toolsproto.StringTemplate{Template: *cfg.Title}
 	}
-	if cfg.HelpText != "" {
-		tool.HelpText = &toolsproto.StringTemplate{Template: cfg.HelpText}
+	if cfg.HelpText != nil {
+		tool.HelpText = &toolsproto.StringTemplate{Template: *cfg.HelpText}
 	}
-	if cfg.Icon != "" {
-		tool.Icon = &cfg.Icon
+	if cfg.Icon != nil {
+		tool.Icon = cfg.Icon
 	}
-	if cfg.EntitySingle != "" {
-		tool.EntitySingle = cfg.EntitySingle
+	if cfg.EntitySingle != nil {
+		tool.EntitySingle = *cfg.EntitySingle
 	}
-	if cfg.EntityPlural != "" {
-		tool.EntityPlural = cfg.EntityPlural
+	if cfg.EntityPlural != nil {
+		tool.EntityPlural = *cfg.EntityPlural
 	}
 	cfg.Capabilities.applyOn(tool)
 
@@ -79,48 +79,110 @@ func (cfg *ToolConfig) applyOn(tool *toolsproto.ActionConfig) {
 			inputCfg.applyOn(toolInput)
 		}
 	}
+	for path, responseCfg := range cfg.Response {
+		if toolResponse := tool.FindResponseByPath(path); toolResponse != nil {
+			responseCfg.applyOn(toolResponse)
+		}
+	}
 }
 
 type InputConfigs map[string]InputConfig
 type InputConfig struct {
-	DisplayName      string `json:"display_name,omitempty"`
-	DisplayOrder     int    `json:"display_order,omitempty"`
-	Visible          bool   `json:"visible,omitempty"`
-	HelpText         string `json:"help_text,omitempty"`
-	Locked           bool   `json:"locked,omitempty"`
-	Placeholder      string `json:"placeholder,omitempty"`
-	VisibleCondition string `json:"visible_condition,omitempty"`
-	SectionName      string `json:"section_name,omitempty"`
+	DisplayName      *string `json:"display_name,omitempty"`
+	DisplayOrder     *int32  `json:"display_order,omitempty"`
+	Visible          *bool   `json:"visible,omitempty"`
+	HelpText         *string `json:"help_text,omitempty"`
+	Locked           *bool   `json:"locked,omitempty"`
+	Placeholder      *string `json:"placeholder,omitempty"`
+	VisibleCondition *string `json:"visible_condition,omitempty"`
+	SectionName      *string `json:"section_name,omitempty"`
 	// TODO: DefaultValue
 	// TODO Lookup Link
 	// TODO GetEntry Link
 }
 
 func (cfg InputConfig) applyOn(input *toolsproto.RequestFieldConfig) {
-	if cfg.DisplayName != "" {
-		input.DisplayName = cfg.DisplayName
+	if cfg.DisplayName != nil {
+		input.DisplayName = *cfg.DisplayName
 	}
-	if cfg.DisplayOrder != int(input.DisplayOrder) {
-		input.DisplayOrder = int32(cfg.DisplayOrder)
+	if cfg.DisplayOrder != nil {
+		input.DisplayOrder = *cfg.DisplayOrder
 	}
-	if cfg.Visible != input.Visible {
-		input.Visible = cfg.Visible
+	if cfg.Visible != nil {
+		input.Visible = *cfg.Visible
 	}
-	if cfg.HelpText != "" {
-		input.HelpText = &toolsproto.StringTemplate{Template: cfg.HelpText}
+	if cfg.HelpText != nil {
+		input.HelpText = &toolsproto.StringTemplate{Template: *cfg.HelpText}
 	}
-	if cfg.Locked != input.Locked {
-		input.Locked = cfg.Locked
+	if cfg.Locked != nil {
+		input.Locked = *cfg.Locked
 	}
-	if cfg.Placeholder != "" {
-		input.Placeholder = &toolsproto.StringTemplate{Template: cfg.Placeholder}
+	if cfg.Placeholder != nil {
+		input.Placeholder = &toolsproto.StringTemplate{Template: *cfg.Placeholder}
 	}
-	if cfg.VisibleCondition != "" {
-		input.VisibleCondition = &cfg.VisibleCondition
+	if cfg.VisibleCondition != nil {
+		input.VisibleCondition = cfg.VisibleCondition
 	}
-	if cfg.SectionName != "" {
-		input.SectionName = &cfg.SectionName
+	if cfg.SectionName != nil {
+		input.SectionName = cfg.SectionName
 	}
+}
+
+func (cfg InputConfig) hasChanges() bool {
+	return cfg.DisplayName != nil ||
+		cfg.DisplayOrder != nil ||
+		cfg.Visible != nil ||
+		cfg.HelpText != nil ||
+		cfg.Locked != nil ||
+		cfg.Placeholder != nil ||
+		cfg.VisibleCondition != nil ||
+		cfg.SectionName != nil
+}
+
+type ResponseConfigs map[string]ResponseConfig
+type ResponseConfig struct {
+	DisplayName      *string `json:"display_name,omitempty"`
+	DisplayOrder     *int32  `json:"display_order,omitempty"`
+	Visible          *bool   `json:"visible,omitempty"`
+	HelpText         *string `json:"help_text,omitempty"`
+	ImagePreview     *bool   `json:"image_preview,omitempty"`
+	VisibleCondition *string `json:"visible_condition,omitempty"`
+	SectionName      *string `json:"section_name,omitempty"`
+	// TODO  Link
+}
+
+func (cfg ResponseConfig) applyOn(response *toolsproto.ResponseFieldConfig) {
+	if cfg.DisplayName != nil {
+		response.DisplayName = *cfg.DisplayName
+	}
+	if cfg.DisplayOrder != nil {
+		response.DisplayOrder = *cfg.DisplayOrder
+	}
+	if cfg.Visible != nil {
+		response.Visible = *cfg.Visible
+	}
+	if cfg.HelpText != nil {
+		response.HelpText = &toolsproto.StringTemplate{Template: *cfg.HelpText}
+	}
+	if cfg.ImagePreview != nil {
+		response.ImagePreview = *cfg.ImagePreview
+	}
+	if cfg.VisibleCondition != nil {
+		response.VisibleCondition = cfg.VisibleCondition
+	}
+	if cfg.SectionName != nil {
+		response.SectionName = cfg.SectionName
+	}
+}
+
+func (cfg ResponseConfig) hasChanges() bool {
+	return cfg.DisplayName != nil ||
+		cfg.DisplayOrder != nil ||
+		cfg.Visible != nil ||
+		cfg.HelpText != nil ||
+		cfg.ImagePreview != nil ||
+		cfg.VisibleCondition != nil ||
+		cfg.SectionName != nil
 }
 
 type Capability string
@@ -153,17 +215,20 @@ func extractConfig(generated, updated *toolsproto.ActionConfig) *ToolConfig {
 		cfg.Name = updated.GetName()
 	}
 	if updated.GetIcon() != generated.GetIcon() {
-		cfg.Icon = updated.GetIcon()
+		cfg.Icon = updated.Icon
 	}
-
-	cfg.Title = generated.GetTitle().Diff(updated.GetTitle())
-	cfg.HelpText = generated.GetHelpText().Diff(updated.GetHelpText())
+	if change := generated.GetTitle().Diff(updated.GetTitle()); change != "" {
+		cfg.Title = &change
+	}
+	if change := generated.GetHelpText().Diff(updated.GetHelpText()); change != "" {
+		cfg.HelpText = &change
+	}
 
 	if updated.GetEntitySingle() != generated.GetEntitySingle() {
-		cfg.EntitySingle = updated.GetEntitySingle()
+		cfg.EntitySingle = &updated.EntitySingle
 	}
 	if updated.GetEntityPlural() != generated.GetEntityPlural() {
-		cfg.EntityPlural = updated.GetEntityPlural()
+		cfg.EntityPlural = &updated.EntityPlural
 	}
 	if caps := generated.GetCapabilities().Diff(updated.GetCapabilities()); len(caps) > 0 {
 		cfg.Capabilities = Capabilities{}
@@ -184,46 +249,81 @@ func extractConfig(generated, updated *toolsproto.ActionConfig) *ToolConfig {
 			cfg.Inputs[updatedInput.GetFieldLocation().GetPath()] = *inputCfg
 		}
 	}
+	cfg.Response = ResponseConfigs{}
+	for _, updatedResponse := range updated.GetResponse() {
+		genResponse := generated.FindResponse(updatedResponse.GetFieldLocation())
+		if genResponse == nil {
+			continue
+		}
+
+		// if we have any response changes, set it on the map
+		if respCfg := extractResponseConfig(genResponse, updatedResponse); respCfg != nil {
+			cfg.Response[updatedResponse.GetFieldLocation().GetPath()] = *respCfg
+		}
+	}
 
 	return cfg
 }
 
 func extractInputConfig(generated, updated *toolsproto.RequestFieldConfig) *InputConfig {
 	cfg := InputConfig{}
-	changed := false
-
 	if generated.DisplayName != updated.DisplayName {
-		cfg.DisplayName = updated.DisplayName
-		changed = true
+		cfg.DisplayName = &updated.DisplayName
 	}
 	if generated.DisplayOrder != updated.DisplayOrder {
-		cfg.DisplayOrder = int(updated.DisplayOrder)
-		changed = true
+		cfg.DisplayOrder = &updated.DisplayOrder
 	}
 	if generated.Visible != updated.Visible {
-		cfg.Visible = updated.Visible
-		changed = true
+		cfg.Visible = &updated.Visible
 	}
 	if generated.Locked != updated.Locked {
-		cfg.Locked = updated.Locked
-		changed = true
+		cfg.Locked = &updated.Locked
 	}
-	if cfg.HelpText = generated.GetHelpText().Diff(updated.GetHelpText()); cfg.HelpText != "" {
-		changed = true
+	if change := generated.GetHelpText().Diff(updated.GetHelpText()); change != "" {
+		cfg.HelpText = &change
 	}
-	if cfg.HelpText = generated.GetPlaceholder().Diff(updated.GetPlaceholder()); cfg.Placeholder != "" {
-		changed = true
+	if change := generated.GetPlaceholder().Diff(updated.GetPlaceholder()); change != "" {
+		cfg.Placeholder = &change
 	}
 	if generated.GetVisibleCondition() != updated.GetVisibleCondition() {
-		cfg.VisibleCondition = updated.GetVisibleCondition()
-		changed = true
+		cfg.VisibleCondition = updated.VisibleCondition
 	}
 	if generated.GetSectionName() != updated.GetSectionName() {
-		cfg.SectionName = updated.GetSectionName()
-		changed = true
+		cfg.SectionName = updated.SectionName
 	}
 
-	if !changed {
+	if !cfg.hasChanges() {
+		return nil
+	}
+
+	return &cfg
+}
+
+func extractResponseConfig(generated, updated *toolsproto.ResponseFieldConfig) *ResponseConfig {
+	cfg := ResponseConfig{}
+	if generated.DisplayName != updated.DisplayName {
+		cfg.DisplayName = &updated.DisplayName
+	}
+	if generated.DisplayOrder != updated.DisplayOrder {
+		cfg.DisplayOrder = &updated.DisplayOrder
+	}
+	if generated.Visible != updated.Visible {
+		cfg.Visible = &updated.Visible
+	}
+	if generated.ImagePreview != updated.ImagePreview {
+		cfg.ImagePreview = &updated.ImagePreview
+	}
+	if change := generated.GetHelpText().Diff(updated.GetHelpText()); change != "" {
+		cfg.HelpText = &change
+	}
+	if generated.GetVisibleCondition() != updated.GetVisibleCondition() {
+		cfg.VisibleCondition = updated.VisibleCondition
+	}
+	if generated.GetSectionName() != updated.GetSectionName() {
+		cfg.SectionName = updated.SectionName
+	}
+
+	if !cfg.hasChanges() {
 		return nil
 	}
 
