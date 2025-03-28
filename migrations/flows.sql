0b72a9532a325b379ae7bde9ec05ffecc6ed5401
Radu Gruia <radu@keel.xyz>
2025-03-28 11:26:26 +0000

    feat: implement GET flow run endpoint (#1789)

--- migrations/flows.sql
+++ migrations/flows.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS keel_flow (
+CREATE TABLE IF NOT EXISTS keel_flow_run (
 	"id" text NOT NULL DEFAULT ksuid() PRIMARY KEY,
 	"name" TEXT NOT NULL,
 	"status" TEXT NOT NULL,
@@ -6,11 +6,11 @@ CREATE TABLE IF NOT EXISTS keel_flow (
 	"created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
 	"updated_at" TIMESTAMPTZ NOT NULL DEFAULT now()
 );
-CREATE OR REPLACE TRIGGER keel_flow_updated_at BEFORE UPDATE ON "keel_flow" FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
+CREATE OR REPLACE TRIGGER keel_flow_run_updated_at BEFORE UPDATE ON "keel_flow_run" FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
 
 CREATE TABLE IF NOT EXISTS keel_flow_step (
 	"id" text NOT NULL DEFAULT ksuid() PRIMARY KEY,
-	"flow_id" text NOT NULL REFERENCES "keel_flow" ("id") ON UPDATE CASCADE ON DELETE CASCADE,
+	"run_id" text NOT NULL REFERENCES "keel_flow_run" ("id") ON UPDATE CASCADE ON DELETE CASCADE,
 	"name" TEXT NOT NULL,
 	"status" TEXT NOT NULL,
 	"type" TEXT NOT NULL,
--- runtime/flows/flow.go
+++ runtime/flows/flow.go
@@ -1,10 +1,18 @@
 package flows
 
 import (
+	"context"
 	"database/sql/driver"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"time"
+
+	"github.com/teamkeel/keel/db"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
+	"gorm.io/gorm"
 )
 
 type Status string
@@ -25,17 +33,33 @@ const (
 	StepTypeWait     StepType = "wait"
 )
 
-type FlowRun struct {
+type Run struct {
 	ID        string    `json:"id" gorm:"primaryKey;not null;default:null"`
 	Status    Status    `json:"status"`
 	Name      string    `json:"name"`
 	Input     *JSONB    `json:"input" gorm:"type:jsonb"`
+	Steps     []Step    `json:"steps"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
 }
 
-func (FlowRun) TableName() string {
-	return "keel_flow"
+func (Run) TableName() string {
+	return "keel_flow_run"
+}
+
+type Step struct {
+	ID        string    `json:"id" gorm:"primaryKey;not null;default:null"`
+	Name      string    `json:"name"`
+	RunID     string    `json:"runId"`
+	Status    Status    `json:"status"`
+	Type      StepType  `json:"type"`
+	Value     *JSONB    `json:"value" gorm:"type:jsonb"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+func (Step) TableName() string {
+	return "keel_flow_step"
 }
 
 // JSONB Interface for JSONB fields
@@ -52,4 +76,43 @@ func (jsonField *JSONB) Scan(value any) error {
 		return errors.New("type assertion to []byte failed")
 	}
 	return json.Unmarshal(data, &jsonField)
+}
+
+// GetFlowRun returns the flow run with the given runID and within the given's scope's flow.
+// If no flow run found, nil nil is returned
+func GetFlowRun(ctx context.Context, scope *Scope, runID string) (*Run, error) {
+	if scope.Flow == nil {
+		return nil, fmt.Errorf("invalid flow")
+	}
+
+	ctx, span := tracer.Start(ctx, "GetFlowRun")
+	defer span.End()
+
+	span.SetAttributes(
+		attribute.String("flow", scope.Flow.Name),
+		attribute.String("flowRunID", runID),
+	)
+
+	database, err := db.GetDatabase(ctx)
+	if err != nil {
+		span.RecordError(err, trace.WithStackTrace(true))
+		span.SetStatus(codes.Error, err.Error())
+		return nil, err
+	}
+
+	var run Run
+
+	result := database.GetDB().Preload("Steps").Where("id = ? and name = ?", runID, scope.Flow.Name).First(&run)
+	if result.Error != nil {
+		span.RecordError(err, trace.WithStackTrace(true))
+		span.SetStatus(codes.Error, result.Error.Error())
+
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+
+		return nil, result.Error
+	}
+
+	return &run, nil
 }
