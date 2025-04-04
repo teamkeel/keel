6447a248eaed8ed21d1c365cb885241e14459259
Dave New <sanodn@gmail.com>
2025-04-04 06:51:25 +0200

    chore: tests

--- integration/testdata/events_basic/tests.test.ts
+++ integration/testdata/events_basic/tests.test.ts
@@ -3,104 +3,106 @@ import { beforeEach, expect, test } from "vitest";
 
 beforeEach(resetDatabase);
 
-// test("events from action", async () => {
-//   await actions.createPerson({ name: "Keelson", email: "keelson@keel.so" });
-//   await actions.createPersonFn({ name: "Weaveton", email: "weaveton@keel.so" });
+test("events from action", async () => {
+  await actions.createPerson({ name: "Keelson", email: "keelson@keel.so" });
+  await actions.createPersonFn({ name: "Weaveton", email: "weaveton@keel.so" });
 
-//   const persons = await models.person.findMany();
+  const persons = await models.person.findMany();
 
-//   expect(persons).toHaveLength(2);
-//   expect(persons[0].verifiedEmail).toBeTruthy();
-//   expect(persons[1].verifiedEmail).toBeTruthy();
-//   expect(persons[0].verifiedUpdate).toBeTruthy();
-//   expect(persons[1].verifiedUpdate).toBeTruthy();
-// });
+  expect(persons).toHaveLength(2);
+  expect(persons[0].verifiedEmail).toBeTruthy();
+  expect(persons[1].verifiedEmail).toBeTruthy();
+  expect(persons[0].verifiedUpdate).toBeTruthy();
+  expect(persons[1].verifiedUpdate).toBeTruthy();
+});
 
-// test("events from hook functions", async () => {
-//   await actions.createPersonFn({ name: "Keelson", email: "keelson@keel.so" });
-//   await actions.createPersonFn({ name: "Weaveton", email: "weaveton@keel.so" });
+test("events from hook functions", async () => {
+  await actions.createPersonFn({ name: "Keelson", email: "keelson@keel.so" });
+  await actions.createPersonFn({ name: "Weaveton", email: "weaveton@keel.so" });
 
-//   const persons = await models.person.findMany();
+  const persons = await models.person.findMany();
 
-//   expect(persons).toHaveLength(2);
-//   expect(persons[0].verifiedEmail).toBeTruthy();
-//   expect(persons[1].verifiedEmail).toBeTruthy();
-//   expect(persons[0].verifiedUpdate).toBeTruthy();
-//   expect(persons[1].verifiedUpdate).toBeTruthy();
-// });
+  expect(persons).toHaveLength(2);
+  expect(persons[0].verifiedEmail).toBeTruthy();
+  expect(persons[1].verifiedEmail).toBeTruthy();
+  expect(persons[0].verifiedUpdate).toBeTruthy();
+  expect(persons[1].verifiedUpdate).toBeTruthy();
+});
 
-// test("events from custom function", async () => {
-//   const result = await actions.writeRandomPersons();
-//   expect(result).toBeTruthy();
+test("events from custom function", async () => {
+  const result = await actions.writeRandomPersons();
+  expect(result).toBeTruthy();
 
-//   const persons = await models.person.findMany();
+  const persons = await models.person.findMany();
 
-//   expect(persons).toHaveLength(2);
-//   expect(persons[0].verifiedEmail).toBeTruthy();
-//   expect(persons[1].verifiedEmail).toBeTruthy();
-//   expect(persons[0].verifiedUpdate).toBeTruthy();
-//   expect(persons[1].verifiedUpdate).toBeTruthy();
-// });
+  expect(persons).toHaveLength(2);
+  expect(persons[0].verifiedEmail).toBeTruthy();
+  expect(persons[1].verifiedEmail).toBeTruthy();
+  expect(persons[0].verifiedUpdate).toBeTruthy();
+  expect(persons[1].verifiedUpdate).toBeTruthy();
+});
 
-// test("events from job", async () => {
-//   await jobs.createRandomPersons({ raiseException: false });
+test("events from job", async () => {
+  await jobs.createRandomPersons({ raiseException: false });
 
-//   const persons = await models.person.findMany();
+  const persons = await models.person.findMany();
 
-//   expect(persons).toHaveLength(2);
-//   expect(persons[0].verifiedEmail).toBeTruthy();
-//   expect(persons[1].verifiedEmail).toBeTruthy();
-//   expect(persons[0].verifiedUpdate).toBeTruthy();
-//   expect(persons[1].verifiedUpdate).toBeTruthy();
-// });
+  expect(persons).toHaveLength(2);
+  expect(persons[0].verifiedEmail).toBeTruthy();
+  expect(persons[1].verifiedEmail).toBeTruthy();
+  expect(persons[0].verifiedUpdate).toBeTruthy();
+  expect(persons[1].verifiedUpdate).toBeTruthy();
+});
 
-// test("events from failed hook function with rollback", async () => {
-//   await expect(
-//     actions.createPersonFn({ name: "", email: "keelson@keel.so" })
-//   ).toHaveError({
-//     code: "ERR_UNKNOWN",
-//   });
+test("events from failed hook function with rollback", async () => {
+  await expect(
+    actions.createPersonFn({ name: "", email: "keelson@keel.so" })
+  ).toHaveError({
+    code: "ERR_UNKNOWN",
+  });
 
-//   const persons = await models.person.findMany();
-//   expect(persons).toHaveLength(0);
-// });
+  const persons = await models.person.findMany();
+  expect(persons).toHaveLength(0);
+});
 
-// test("events from failed job", async () => {
-//   await jobs.createRandomPersons({ raiseException: true });
+test("events from failed job", async () => {
+  await jobs.createRandomPersons({ raiseException: true });
 
-//   const persons = await models.person.findMany();
+  const persons = await models.person.findMany();
 
-//   expect(persons).toHaveLength(2);
-//   expect(persons[0].verifiedEmail).toBeTruthy();
-//   expect(persons[1].verifiedEmail).toBeTruthy();
-//   expect(persons[0].verifiedUpdate).toBeTruthy();
-//   expect(persons[1].verifiedUpdate).toBeTruthy();
-// });
+  expect(persons).toHaveLength(2);
+  expect(persons[0].verifiedEmail).toBeTruthy();
+  expect(persons[1].verifiedEmail).toBeTruthy();
+  expect(persons[0].verifiedUpdate).toBeTruthy();
+  expect(persons[1].verifiedUpdate).toBeTruthy();
+});
 
-// test("events from failed custom function with rollback", async () => {
-//   await expect(
-//     actions.writeRandomPersons({ raiseException: true })
-//   ).toHaveError({
-//     code: "ERR_UNKNOWN",
-//   });
+test("events from failed custom function with rollback", async () => {
+  await expect(
+    actions.writeRandomPersons({ raiseException: true })
+  ).toHaveError({
+    code: "ERR_UNKNOWN",
+  });
 
-//   const persons = await models.person.findMany();
-//   expect(persons).toHaveLength(0);
-// });
+  const persons = await models.person.findMany();
+  expect(persons).toHaveLength(0);
+});
 
-// test("event previous data", async () => {
-//   const t = await models.tracker.create({
-//     views: 0,
-//   });
+test("event previous data", async () => {
+  const t = await models.tracker.create({
+    views: 0,
+  });
 
-//   const updated = await actions.updateViews({ where: { id: t.id }, values: { views: 1 } });
+  const updated = await actions.updateViews({
+    where: { id: t.id },
+    values: { views: 1 },
+  });
 
-//   const get = await models.tracker.findOne({ id: t.id });
+  const get = await models.tracker.findOne({ id: t.id });
 
-//   expect(get!.views).toEqual(1);
-//   expect(get!.verifiedUpdate).toBeTruthy();
-
-// });
+  expect(get!.views).toEqual(1);
+  expect(get!.verifiedUpdate).toBeTruthy();
+});
 
 test("event previous data bulk", async () => {
   await models.tracker.create({
