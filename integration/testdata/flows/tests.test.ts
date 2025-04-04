886dbdcf43da456aec2d5c9fe7bfdf022fde9571
Radu Gruia <radu@keel.xyz>
2025-04-04 14:29:13 +0100

    test: assert flows db tables are populated

--- integration/testdata/flows/tests.test.ts
+++ integration/testdata/flows/tests.test.ts
@@ -1,5 +1,7 @@
-import { actions, resetDatabase, models, flows } from "@teamkeel/testing";
+import { resetDatabase, models, flows } from "@teamkeel/testing";
+import { useDatabase } from "@teamkeel/sdk"
 import { beforeEach, expect, test } from "vitest";
+import { sql } from "kysely";
 
 beforeEach(resetDatabase);
 
@@ -9,4 +11,9 @@ test("flows - basic execution", async () => {
   const things = await models.thing.findMany();
   expect(things.length).toBe(1);
   expect(things[0].name).toBe("Keelson");
+  
+  const dbFlows = await sql`SELECT * FROM keel_flow_run`.execute(useDatabase());
+  expect(dbFlows.rows.length).toBe(1);
+  const dbSteps = await sql`SELECT * FROM keel_flow_step`.execute(useDatabase());
+  expect(dbSteps.rows.length).toBe(2);
 });
