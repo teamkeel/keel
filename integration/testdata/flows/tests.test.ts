08283e4365099945d7efbe70fa7f542ab886def0
Radu Gruia <radu@keel.xyz>
2025-04-04 14:32:01 +0100

    style: prettier

--- integration/testdata/flows/tests.test.ts
+++ integration/testdata/flows/tests.test.ts
@@ -1,5 +1,5 @@
 import { resetDatabase, models, flows } from "@teamkeel/testing";
-import { useDatabase } from "@teamkeel/sdk"
+import { useDatabase } from "@teamkeel/sdk";
 import { beforeEach, expect, test } from "vitest";
 import { sql } from "kysely";
 
@@ -11,9 +11,11 @@ test("flows - basic execution", async () => {
   const things = await models.thing.findMany();
   expect(things.length).toBe(1);
   expect(things[0].name).toBe("Keelson");
-  
+
   const dbFlows = await sql`SELECT * FROM keel_flow_run`.execute(useDatabase());
   expect(dbFlows.rows.length).toBe(1);
-  const dbSteps = await sql`SELECT * FROM keel_flow_step`.execute(useDatabase());
+  const dbSteps = await sql`SELECT * FROM keel_flow_step`.execute(
+    useDatabase()
+  );
   expect(dbSteps.rows.length).toBe(2);
 });
