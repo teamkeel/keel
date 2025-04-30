2f66491dd0b47c0c96bfb3e4c389aab48cb068b9
tomfrew <tom@keel.xyz>
2025-04-30 15:58:31 +0100

    fix: functions runtime exports

--- packages/functions-runtime/src/index.ts
+++ packages/functions-runtime/src/index.ts
@@ -1,6 +1,46 @@
-// Export JS
-export * from "./index.js";
-export * from "./types";
+import { ModelAPI } from "./ModelAPI";
+import { RequestHeaders } from "./RequestHeaders";
+import { handleRequest } from "./handleRequest";
+import { handleJob } from "./handleJob";
+import { handleSubscriber } from "./handleSubscriber";
+import { handleRoute } from "./handleRoute";
+import { handleFlow } from "./handleFlow";
+import KSUID from "ksuid";
+import { useDatabase } from "./database";
+import {
+  Permissions,
+  PERMISSION_STATE,
+  checkBuiltInPermissions,
+} from "./permissions";
+import * as tracing from "./tracing";
+import { InlineFile, File } from "./File";
+import { Duration } from "./Duration";
+import { ErrorPresets } from "./errors";
+
+export {
+  ModelAPI,
+  RequestHeaders,
+  handleRequest,
+  handleJob,
+  handleSubscriber,
+  handleRoute,
+  handleFlow,
+  KSUID,
+  useDatabase,
+  Permissions,
+  PERMISSION_STATE,
+  checkBuiltInPermissions,
+  tracing,
+  InlineFile,
+  File,
+  Duration,
+  ErrorPresets,
+};
+
+export function ksuid() {
+  return KSUID.randomSync().string;
+}
 
 // Export TS
+export * from "./types";
 export { UI, StepContext, FlowConfig, FlowFunction } from "./flows";
