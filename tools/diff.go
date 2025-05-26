812b1c364fa2a0166e6c92d78cd6f53ab625fe9b
Dave New <sanodn@gmail.com>
2025-05-26 16:41:12 +0200

    fix: unnecessary tool writes

--- tools/diff.go
+++ tools/diff.go
@@ -261,6 +261,10 @@ func extractPaginationConfig(generated, updated *toolsproto.CursorPaginationConf
 		return nil
 	}
 
+	if generated != nil && updated != nil {
+		return nil
+	}
+
 	if generated == nil && updated != nil {
 		if updated.GetPageSize() == nil {
 			return nil
