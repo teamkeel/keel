658d9ecd856607c8671467d4230e6c651546c6d3
Radu Gruia <radu@keel.xyz>
2025-06-18 14:24:10 +0100

    test: add integration test

--- integration/testdata/flows/flows/envStep.ts
+++ integration/testdata/flows/flows/envStep.ts
@@ -4,4 +4,8 @@ export default EnvStep({}, async (ctx) => {
   await ctx.step("env step", async () => {
     return ctx.env.PERSON_NAME;
   });
+
+  await ctx.step("identity step", async () => {
+    return ctx.identity.email;
+  });
 });
