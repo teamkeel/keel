f44d932ec6966aae09a0ec80ab96bbf8c2b71d38
Radu Gruia <radu@keel.xyz>
2025-08-06 12:06:47 +0100

    test: add test for user defined delays

---
+++ integration/testdata/flows_errors/flows/userDelays.ts
@@ -0,0 +1,27 @@
+import { UserDelays, FlowConfig } from "@teamkeel/sdk";
+
+export default UserDelays({}, async (ctx) => {
+  await ctx.step(
+    "user defined delay step",
+    {
+      retries: 3,
+      retryDelay: (retry: number) => {
+        switch (retry) {
+          case 1:
+            return 3000; // retry 1, delay 3s
+          case 2:
+            return 1000; // retry 2, delay 1s
+          default:
+            return 2000; // retry 3, delay 2s
+        }
+      },
+    },
+    async (args) => {
+      if (args.attempt !== 4) {
+        throw new Error("enforce 3 retries");
+      }
+
+      return "completed";
+    }
+  );
+});
