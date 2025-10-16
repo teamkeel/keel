e063a5adc2d8af357a99d2e672d7e31aded38387
Radu Gruia <radu@keel.xyz>
2025-10-16 10:56:22 +0100

    feat: add datepciker flow input element

---
+++ packages/functions-runtime/src/flows/ui/elements/input/datePicker.ts
@@ -0,0 +1,57 @@
+import {
+  BaseUiInputResponse,
+  InputElementImplementation,
+  InputElement,
+} from "../..";
+
+type ElementDataType = string;
+
+/**
+ * Defines what type of datepicker should we use.
+ * @default "dateTime"
+ */
+type DatepickerMode =
+  /** Pick both a date and a time */
+  | "dateTime"
+  /** Pick only a date */
+  | "date";
+
+export type UiElementInputDatePicker = InputElement<
+  ElementDataType,
+  {
+    mode?: DatepickerMode;
+    /** Allows selecting only past dates/datetimes. */
+    pastOnly?: boolean;
+  }
+>;
+
+// The shape of the response over the API
+export interface UiElementInputDatePickerApiResponse
+  extends BaseUiInputResponse<"ui.input.datepicker", ElementDataType> {
+  mode?: DatepickerMode;
+  pastOnly?: boolean;
+}
+
+export const datePickerInput: InputElementImplementation<
+  ElementDataType,
+  UiElementInputDatePicker,
+  UiElementInputDatePickerApiResponse
+> = (name, options) => {
+  return {
+    __type: "input",
+    uiConfig: {
+      __type: "ui.input.datepicker",
+      name,
+      label: options?.label || name,
+      optional: options?.optional || false,
+      disabled: options?.disabled || false,
+      helpText: options?.helpText,
+      defaultValue: options?.defaultValue,
+      mode: options?.mode,
+      pastOnly: options?.pastOnly,
+    } satisfies UiElementInputDatePickerApiResponse,
+    validate: options?.validate,
+    onLeave: options?.onLeave,
+    getData: (x: ElementDataType) => x,
+  };
+};
