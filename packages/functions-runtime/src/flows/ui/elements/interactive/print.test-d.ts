93270fbf79dd47516080834ed596b4c45ea76fd1
tomfrew <tom@keel.xyz>
2025-11-05 17:02:33 +0000

    feat: support rawPdf print type (#2054)

--- packages/functions-runtime/src/flows/ui/elements/interactive/print.test-d.ts
+++ packages/functions-runtime/src/flows/ui/elements/interactive/print.test-d.ts
@@ -49,10 +49,41 @@ describe("print element", () => {
 
       ctx.ui.interactive.print({
         jobs: [
+          {
+            type: "rawPdf",
+            url: "http://example.com",
+          },
+        ],
+      });
+
+      ctx.ui.interactive.print({
+        jobs: [
+          {
+            type: "rawPdf",
+            url: "http://example.com",
+            dpi: 300,
+            pageWidth: 1200,
+            pageHeight: 1800,
+          },
+        ],
+      });
+
+      ctx.ui.interactive.print({
+        jobs: [
           // @ts-expect-error - can't have both data and url
           {
             type: "zpl",
             url: "http://example.com",
+            data: "test",
+          },
+        ],
+      });
+
+      ctx.ui.interactive.print({
+        jobs: [
+          // @ts-expect-error - can't have rawPdf with data
+          {
+            type: "rawPdf",
             data: "test",
           },
         ],
