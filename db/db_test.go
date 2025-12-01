d11695638de46637e70cfc2e3abb26332d151801
Radu Gruia <radu@keel.xyz>
2025-12-01 11:18:07 +0000

    fix: linter

--- db/db_test.go
+++ db/db_test.go
@@ -34,7 +34,7 @@ func TestDbTransactionConcurrency(t *testing.T) {
 	_, err := db.ExecuteStatement(ctx, "DROP TABLE IF EXISTS testdbtransactionconcurrency")
 	assert.NoError(t, err)
 	t.Cleanup(func() {
-		//nolint:usetesting use background ctx for clearing db
+		//nolint:usetesting
 		_, err = db.ExecuteStatement(context.Background(), "DROP TABLE IF EXISTS testdbtransactionconcurrency")
 		assert.NoError(t, err)
 	})
